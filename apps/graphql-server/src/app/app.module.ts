ab93de0f15a0905166adecc845276851deabdf75
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-01-19 17:14:21 +0100

    feat: create createAdminUser mutation

--- apps/graphql-server/src/app/app.module.ts
+++ apps/graphql-server/src/app/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 import { ApiGraphqlResolversModule } from '@bgap/api/graphql/resolvers';
+
+const SCHEMA_ROOT = join(process.cwd(), 'libs/api/graphql/schema/src');
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      typePaths: [`${SCHEMA_ROOT}/schema/**/*.graphql`],
       installSubscriptionHandlers: true,
+      // https://docs.nestjs.com/graphql/subscriptions#customize-subscriptions-server
+      // subscriptions: {
+      //   keepAlive: 5000,
+      // },
     }),
     ApiGraphqlResolversModule,
   ],
