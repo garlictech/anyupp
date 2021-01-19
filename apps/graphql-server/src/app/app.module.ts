f853996b04d139432817aab255dc984b6c8268a2
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-01-19 17:01:23 +0100

    core: creat graphql typeings generator script

--- apps/graphql-server/src/app/app.module.ts
+++ apps/graphql-server/src/app/app.module.ts
@@ -2,24 +2,14 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { join } from 'path';
 import { ApiGraphqlResolversModule } from '@bgap/api/graphql/resolvers';
-
-const SCHEMA_ROOT = join(process.cwd(), 'libs/api/graphql/schema/src');
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      typePaths: [`${SCHEMA_ROOT}/schema/**/*.graphql`],
-      definitions: {
-        path: `${SCHEMA_ROOT}/lib/api.ts`,
-        emitTypenameField: true,
-        outputAs: 'class',
-      },
       installSubscriptionHandlers: true,
-
     }),
-    ApiGraphqlResolversModule
+    ApiGraphqlResolversModule,
   ],
   controllers: [AppController],
   providers: [AppService],
