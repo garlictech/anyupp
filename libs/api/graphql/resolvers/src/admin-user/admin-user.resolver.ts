8887237bb0676b17eb796bac8f36b69cf44031ad
Tamás Petró <petro.tamas@gmail.com>
2021-01-21 14:23:12 +0100

    Add inactive role to AdminUserCreate

--- libs/api/graphql/resolvers/src/admin-user/admin-user.resolver.ts
+++ libs/api/graphql/resolvers/src/admin-user/admin-user.resolver.ts
@@ -6,6 +6,7 @@ import {
   CreateAdminUserInput,
   UpdateAdminUserInput,
 } from '@bgap/api/graphql/schema';
+import { EAdminRole } from '@bgap/shared/types/enums';
 import { Inject } from '@nestjs/common';
 
 @Resolver('AdminUser')
@@ -50,12 +51,17 @@ export class AdminUserResolver {
   ): Promise<boolean> {
     const user = await admin.auth().createUser({
       ...newAdminData,
-      password: Math.random().toString(36).substring(2, 10),
+      password: Math.random().toString(36).substring(2, 10)
     });
     return admin
       .database()
       .ref(`adminUsers/${user.uid}`)
-      .update(newAdminData)
+      .update({
+        ...newAdminData,
+        roles: {
+          role: EAdminRole.INACTIVE
+        }
+      })
       .then(() => true);
   }
 
