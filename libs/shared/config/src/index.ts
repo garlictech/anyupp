edaec1d78081ac3aade99d315c752d72de611e28
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-02-12 19:40:19 +0100

    fix: firebaseConfig

--- libs/shared/config/src/index.ts
+++ libs/shared/config/src/index.ts
@@ -1,11 +1,17 @@
 import * as firebaseConfig from './lib/config/firebase.config.json';
 import * as config from './lib/config/config.json';
 import * as FIREBASE_SERVICE_ACCOUNT from './lib/config/firebase-service-account-key.json';
-import { SharedConfig, SharedFirebaseConfig } from './lib/interfaces';
 
-const fbConf: SharedFirebaseConfig = firebaseConfig;
-const conf: SharedConfig = config;
-export { fbConf as FIREBASE_CONFIG, conf as CONFIG, FIREBASE_SERVICE_ACCOUNT };
+// TODO: ok exporting fbConf instead of firebaseConfig we got this error: Error: Can't determine Firebase Database URL.
+// import { SharedConfig, SharedFirebaseConfig } from './lib/interfaces';
+
+// const fbConf: SharedFirebaseConfig = firebaseConfig;
+// const conf: SharedConfig = config;
+export {
+  firebaseConfig as FIREBASE_CONFIG,
+  config as CONFIG,
+  FIREBASE_SERVICE_ACCOUNT,
+};
 
 export * from './lib/shared-secrets.module';
 export * from './lib/interfaces';
