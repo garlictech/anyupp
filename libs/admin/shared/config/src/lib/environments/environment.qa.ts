56de90d0ddc754eced9c253a46f19643d7a1bdc9
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-02-25 15:25:09 +0100

    chore: dummy qa environment

--- libs/admin/shared/config/src/lib/environments/environment.qa.ts
+++ libs/admin/shared/config/src/lib/environments/environment.qa.ts
@@ -4,5 +4,11 @@ export const environment = {
   production: true,
   id: 'qa',
   name: 'QA',
+  gql: {
+    // uri: `https://graphql-inmg2ygdca-uc.a.run.app/graphql`,
+    http: 'http://localhost:3333/graphql',
+    // uri: `wss://graphql-inmg2ygdca-uc.a.run.app/graphql`,
+    ws: 'ws://localhost:3333/graphql',
+  },
   config: CONFIG,
 };
