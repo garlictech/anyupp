fad3f2b48647304cc3dfda0c88584fb82a47ea96
Tamás Petró <petro.tamas@gmail.com>
2021-03-01 15:16:46 +0100

    Package fix

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import Amplify  from 'aws-amplify';
+import Amplify  from '@aws-amplify/core';
 import { AppModule } from './app/app.module';
 import { environment } from '@bgap/admin/shared/config';
 import { AWS_CONFIG } from '@bgap/admin/shared/config';
