0f098710bc4d0737dc13ea175433048478d8b265
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-03-12 22:48:40 +0100

    ci: aws user in secret manager

--- tools/setup-aws-environment.sh
+++ tools/setup-aws-environment.sh
@@ -2,17 +2,8 @@
 set -e
 IFS='|'
 
-aws configure set region $AWS_REGION
-echo "***** $AWS_PROFILE"
-
-echo "***** $AWS_SECRET_ACCESS_KEY"
-echo "*****1"
-aws configure set output "json"
-echo "*****2"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-echo "*****3"
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-echo "*****4"
-aws configure set aws_session_token "$AWS_SESSION_TOKEN"
-
-echo "*****5"
+aws configure --profile $AWS_PROFILE set region $AWS_REGION
+aws configure --profile $AWS_PROFILE set output "json"
+aws configure --profile $AWS_PROFILE set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure --profile $AWS_PROFILE set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure --profile $AWS_PROFILE set aws_session_token "$AWS_SESSION_TOKEN"
