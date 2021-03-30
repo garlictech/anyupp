3a18e74c5347733f1764cf32d058ef78b7306812
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-03-30 19:58:55 +0200

    fix: fail the build in case of unhandled rejection

--- apps/anyupp-mobile/lib/awsconfiguration.dart
+++ apps/anyupp-mobile/lib/awsconfiguration.dart
@@ -1,9 +1,11 @@
 const AWSCONFIG = '''{
-  "GraphqlApiKey": "da2-gvjc35ddcrg5jkopklmnm5lsga",
-  "GraphqlApiUrl": "https://cfclp5ha5valbary2hf44ar3fi.appsync-api.eu-west-1.amazonaws.com/graphql",
-  "IdentityPoolId": "eu-west-1:0094519b-3ba3-4141-949e-11bbe66b4ebc",
-  "consumerNativeUserPoolClientId": "5nleancp5e8puouskmt9sluvrp",
-  "consumerUserPoolDomain": "https://qa-anyupp-backend-consumer.auth.eu-west-1.amazoncognito.com",
-  "consumerUserPoolId": "eu-west-1_5lS3NTg2S",
-  "stripePublishableKey": "pk_test_51H6vUFKnnCp2ddwxrks1Md8euzovN1gxvvKsXBVBwXumf70MfdLOJGkZN7cz4Rz3n1Dt5DD0UBHxkRuAHcaVZxM900rz9vDB"
+  "GraphqlApiKey": "da2-hishj2u535fqte66kc3libjyhy",
+  "GraphqlApiUrl": "https://zoaztynnbrgotnwsll37vkvtvi.appsync-api.eu-west-1.amazonaws.com/graphql",
+  "GraphqlWebsocketApiUrl": "wss://g7rewo2kyrfnvbubzbbyhwplxm.appsync-realtime-api.eu-west-1.amazonaws.com/graphql",
+  "IdentityPoolId": "eu-west-1:f90cc6c3-221f-4834-8dd4-d8ab2b913e0d",
+  "consumerNativeUserPoolClientId": "4nmjm2mfulr7du7houfogc0vns",
+  "consumerUserPoolDomain": "https://dev-anyupp-backend-consumer.auth.eu-west-1.amazoncognito.com",
+  "consumerUserPoolId": "eu-west-1_yKkBXvlqI",
+  "region": "eu-west-1",
+  "stripePublishableKey": "pk_test_51H6vUFKnnCp2ddwxrks1Md8euzovN1gxvvKsXBVBwXumf70MfdLOJGkZN7cz4Rz3n1Dt5DD0UBHxkRuAHcaVZxM900rz9vDBds"
 }''';
