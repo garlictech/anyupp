f7fafe38866ec4e201323edc305e92b30967cab3
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-03-31 13:18:01 +0200

    build: fix import mode to prevent circular dep build problem

--- libs/shared/types/src/lib/validators/custom-query-documents.ts
+++ libs/shared/types/src/lib/validators/custom-query-documents.ts
@@ -1,4 +1,5 @@
-import { groupSchema, IGroup, validateSchema } from '@bgap/shared/types';
+import { validateSchema } from '../../lib/validation/validate';
+import { IGroup, groupSchema } from '../interfaces';
 
 export const { validate: validateGetGroupCurrency } = validateSchema<IGroup>(
   { currency: groupSchema.currency, __typename: groupSchema.__typename },
