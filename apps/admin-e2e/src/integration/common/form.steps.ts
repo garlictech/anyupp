3fe1d4009b6deb369f60e206afcdf73733ba09af
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-04-09 19:15:27 +0200

    test(e2e): be able to login with the test user into the admin page

--- apps/admin-e2e/src/integration/common/form.steps.ts
+++ apps/admin-e2e/src/integration/common/form.steps.ts
@@ -1,8 +1,22 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
 
+// When(
+//   'I fill out the {string} input with {string}',
+//   (inputLabel: string, value: string) => {
+//     cy.findByLabelText(inputLabel).type(value, { force: true });
+//   },
+// );
+
+// When(
+//   'I fill out the input with {string} as placeholder with {string}',
+//   (placeholder: string, value: string) => {
+//     cy.findByPlaceholderText(placeholder).type(value);
+//   },
+// );
+
 When(
-  'I fill out the {string} input with {string}',
-  (inputLabel: string, value: string) => {
-    cy.findByLabelText(inputLabel).type(value, { force: true });
+  'I fill out the input with id {string} with the {string} value',
+  (id: string, value: string) => {
+    cy.get(`#${id}`).type(value, { force: true });
   },
 );
