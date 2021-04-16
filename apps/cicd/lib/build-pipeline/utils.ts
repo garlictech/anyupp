fb35117f380dfaa2bdba529db1ebe2d8c94e9d29
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-04-16 14:59:32 +0200

    ci: stack creation fix #3

--- apps/cicd/lib/build-pipeline/utils.ts
+++ apps/cicd/lib/build-pipeline/utils.ts
@@ -84,7 +84,7 @@ export const createBuildProject = (
           commands: [
             `yarn nx config crud-backend --app=${appConfig.name} --stage=${stage}`,
             `yarn nx config shared-config --app=${appConfig.name} --stage=${stage}`,
-            `yarn nx build anyupp-gql-api --skip-nx-cach`,
+            `yarn nx build anyupp-gql-api --skip-nx-cache`,
           ],
         },
         build: {
