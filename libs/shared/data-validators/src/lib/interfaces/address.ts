892c9ed712db22954765757a217a7d26cfdb7e46
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-04-19 16:26:31 +0200

    test: fix integration tests

--- libs/shared/data-validators/src/lib/interfaces/address.ts
+++ libs/shared/data-validators/src/lib/interfaces/address.ts
@@ -1,25 +1,21 @@
 import * as Joi from 'joi';
 import { IAddress, IAddressInfo, ILocation } from '@bgap/shared/types';
-
 export const latitudeSchema = Joi.number().min(-90).max(90).precision(8);
 export const longitudeSchema = Joi.number().min(-180).max(180).precision(8);
-
 export const locationSchema: Joi.SchemaMap<ILocation> = {
   __typename: Joi.string().valid('Location').optional(),
   lat: latitudeSchema.required(),
   lng: longitudeSchema.required(),
 };
-
 export const addressSchema: Joi.SchemaMap<IAddress> = {
   __typename: Joi.string().valid('Address').optional(),
-  address: Joi.string(),
-  city: Joi.string(),
-  country: Joi.string(),
-  title: Joi.string(),
-  postalCode: Joi.string(),
-  location: Joi.object(locationSchema),
+  address: Joi.string().allow(null),
+  city: Joi.string().allow(null),
+  country: Joi.string().allow(null),
+  title: Joi.string().allow(null),
+  postalCode: Joi.string().allow(null),
+  location: Joi.object(locationSchema).allow(null),
 };
-
 export const addressInfoSchema: Joi.SchemaMap<IAddressInfo> = {
   address: Joi.object(addressSchema).required(),
 };
