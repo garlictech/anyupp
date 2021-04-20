31c7a0cff31638d10bf9923d8c967678dbcf9c78
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-04-20 23:56:30 +0200

    ci: optimize CI build #2

--- apps/anyupp-mobile/ios/appcenter-post-clone.sh
+++ apps/anyupp-mobile/ios/appcenter-post-clone.sh
@@ -9,19 +9,10 @@ set -x
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 
-echo "*********"
-echo $PWD
-ls -l
-
 cd ..
 git clone -b beta https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 echo "Installed flutter to `pwd`/flutter"
-
-echo "*********2"
-echo $PWD
-ls -l
-
 
 flutter channel stable
 flutter doctor
@@ -32,11 +23,6 @@ echo "***** The build: ${APPCENTER_BRANCH}/${ARTIFACT_NAME}"
 cd ../..
 aws s3 cp s3://anyupp-build-artifacts-${APPCENTER_BRANCH}/${ARTIFACT_NAME} .
 tar -zxf ${ARTIFACT_NAME}
-ls -l apps/anyupp-mobile/lib
-
-echo "*********3"
-echo $PWD
-ls -l
 
 cd apps/anyupp-mobile
 flutter build ios --release --no-codesign
