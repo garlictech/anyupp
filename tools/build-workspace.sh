7fe2e5aa1f4ba7b5c75d3d61003091d410b78a88
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-04-21 13:17:06 +0200

    ci: new general site certificate, CI fixes #2

--- tools/build-workspace.sh
+++ tools/build-workspace.sh
@@ -5,16 +5,16 @@ IFS='|'
 APPNAME=$1
 STAGE=$2
 
-nx config crud-backend --app=$APPNAME --stage=$STAGE
-nx config shared-config --app=$APPNAME --stage=$STAGE
-nx build anyupp-gql-api --skip-nx-cache
-nx build-schema crud-backend --app=$APPNAME --stage=$STAGE
-nx build-schema crud-backend --app=$APPNAME --stage=$STAGE
-nx build anyupp-backend --app=$APPNAME --stage=$STAGE --skip-nx-cache
+yarn nx config crud-backend --app=$APPNAME --stage=$STAGE
+yarn nx config shared-config --app=$APPNAME --stage=$STAGE
+yarn nx build anyupp-gql-api --skip-nx-cache
+yarn nx build-schema crud-backend --app=$APPNAME --stage=$STAGE
+yarn nx build-schema crud-backend --app=$APPNAME --stage=$STAGE
+yarn nx build anyupp-backend --app=$APPNAME --stage=$STAGE --skip-nx-cache
 
 if [ $STAGE = 'dev' ]; then
-  nx build admin
+  yarn nx build admin
 else
-  nx build admin --configuration=$STAGE --skip-nx-cache
+  yarn nx build admin --configuration=$STAGE --skip-nx-cache
 fi
 
