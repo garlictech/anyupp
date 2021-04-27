bbf0ac680329e162593b83c036093637d556ed48
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-04-27 10:21:06 +0200

    feat: be able to delete/create more than 25 generatedProduct in a batch dynamodb write operation

--- libs/anyupp-gql/backend/src/lib/utils/dynamodb.utils.ts
+++ libs/anyupp-gql/backend/src/lib/utils/dynamodb.utils.ts
@@ -1,14 +1,29 @@
+// TODO: further optimization could be achieved working with the metadata from the batch result
+// {
+//   "$metadata": {
+//     "httpStatusCode": 200,
+//     "requestId": "QAMPE2D05F8MJ59BHL48RAVJ97VV4KQNSO5AEMVJF66Q9ASUAAJG",
+//     "attempts": 1,
+//     "totalRetryDelay": 0
+//   },
+//   "UnprocessedItems": {}
+import { DateTime } from 'luxon';
 import { from } from 'rxjs';
+import { bufferCount, last, map, mergeMap, scan } from 'rxjs/operators';
 
+// }
 import {
   BatchWriteItemCommand,
+  BatchWriteItemCommandOutput,
   BatchWriteItemInput,
   DynamoDBClient,
   WriteRequest,
 } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
 import { AWS_CRUD_CONFIG } from '@bgap/shared/graphql/api-client';
-import { DateTime } from 'luxon';
+
+const DYNAMODB_BATCH_WRITE_ITEM_COUNT = 25;
+const DYNAMODB_CONCURRENT_OPERATION_COUNT = 2;
 
 const toBatchDeleteParam = (id: string): WriteRequest => ({
   DeleteRequest: { Key: { id: { S: id } } },
@@ -25,7 +40,7 @@ const toBatchPutParam = (item: any): WriteRequest => ({
   },
 });
 
-// TODO: split to chunks by 25 item + extract the logic and test it
+/** Returns all the results in a single emission */
 const executeBatchWrite = (tablename: string) => (
   writeRequests: WriteRequest[],
 ) => {
@@ -33,21 +48,30 @@ const executeBatchWrite = (tablename: string) => (
     region: AWS_CRUD_CONFIG.aws_appsync_region,
   });
 
-  const params: BatchWriteItemInput = {
-    RequestItems: {
-      [tablename]: writeRequests,
-    },
-  };
-
-  // return from(dynamodb.batchWrite(params).promise());
-  return from(dbClient.send(new BatchWriteItemCommand(params)));
+  return from(writeRequests).pipe(
+    // SPLIT the operations into fix sized chunks
+    bufferCount(DYNAMODB_BATCH_WRITE_ITEM_COUNT),
+    map(toBatchWriteIteminput(tablename)),
+    // EXECUTE fix amount of the operation concurrently
+    mergeMap(
+      params => from(dbClient.send(new BatchWriteItemCommand(params))),
+      DYNAMODB_CONCURRENT_OPERATION_COUNT,
+    ),
+    scan((acc: BatchWriteItemCommandOutput[], curr) => [...acc, curr], []),
+    last(),
+  );
 };
+const toBatchWriteIteminput = (tablename: string) => (
+  writeRequests: WriteRequest[],
+): BatchWriteItemInput => ({
+  RequestItems: {
+    [tablename]: writeRequests,
+  },
+});
 
-// TODO: split to chunks by 25 item + extract the logic and test it
 export const executeBatchDelete = (tablename: string) => (ids: Array<string>) =>
   executeBatchWrite(tablename)(ids.map(toBatchDeleteParam));
 
-// TODO: split to chunks by 25 item + extract the logic and test it
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const executeBatchPut = (tablename: string) => (items: Array<any>) =>
   executeBatchWrite(tablename)(items.map(toBatchPutParam));
--- libs/integration-tests/universal/src/lib/libs/anyupp-gql/product/generated-product.spec.ts
+++ libs/integration-tests/universal/src/lib/libs/anyupp-gql/product/generated-product.spec.ts
@@ -6,7 +6,7 @@ import {
 import { crudBackendGraphQLClient } from '@bgap/shared/graphql/api-client';
 import { getSortedIds } from '@bgap/shared/utils';
 import { combineLatest, of } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { scan, switchMap, tap } from 'rxjs/operators';
 import { generatedProductSeed } from '@bgap/shared/fixtures';
 import {
   createTestGeneratedProduct,
@@ -15,14 +15,10 @@ import {
 
 const unitId_01 = 'UNIT_ID_01';
 const unitId_02 = 'UNIT_ID_02';
+const unitId_03 = 'UNIT_ID_03';
 const unit01_generatedProduct_01 = {
   ...generatedProductSeed.base,
   id: `generatedProduct_u${unitId_01}_01`,
-  unitId: unitId_01,
-};
-const unit01_generatedProduct_02 = {
-  ...generatedProductSeed.base,
-  id: `generatedProduct_u${unitId_01}_02`,
   unitId: unitId_01,
 };
 const unit02_generatedProduct_01 = {
@@ -30,30 +26,32 @@ const unit02_generatedProduct_01 = {
   id: `generatedProduct_u${unitId_02}_01`,
   unitId: unitId_02,
 };
+const unit03_generatedProduct_01 = {
+  ...generatedProductSeed.base,
+  id: `generatedProduct_u${unitId_03}_01`,
+  unitId: unitId_03,
+};
+const unit03_generatedProduct_02 = {
+  ...generatedProductSeed.base,
+  id: `generatedProduct_u${unitId_03}_02`,
+  unitId: unitId_03,
+};
 
-const { id, ...unit01_generatedProduct_WITHOUTID } = unit01_generatedProduct_01;
+const PRODUCT_NUM_FOR_BATCH_CRUD = 99;
+const productIds = [...Array(PRODUCT_NUM_FOR_BATCH_CRUD).keys()]
+  .map(id => id.toString().padStart(2, '0'))
+  .map(id => `ID_${id}`);
 
 describe('GenerateProduct tests', () => {
   beforeAll(async () => {
-    // authHelper = await createAuthenticatedAnyuppGraphQLClient(
-    //   testAdminUsername,
-    //   testAdminUserPassword,
-    // ).toPromise();
-    // console.warn(authHelper.userAttributes);
-
     await combineLatest([
       // CleanUP
-      // // generated
-      // deleteTestGeneratedProduct(unit01_generatedProduct_01.id),
-      // deleteTestGeneratedProduct(unit01_generatedProduct_02.id),
       deleteTestGeneratedProduct(unit02_generatedProduct_01.id),
     ])
       .pipe(
         switchMap(() =>
           // Seeding
           combineLatest([
-            // createTestGeneratedProduct(unit01_generatedProduct_01),
-            // createTestGeneratedProduct(unit01_generatedProduct_02),
             createTestGeneratedProduct(unit02_generatedProduct_01),
           ]),
         ),
@@ -61,30 +59,91 @@ describe('GenerateProduct tests', () => {
       .toPromise();
   }, 25000);
 
-  // describe('generateProductListForAUnit function', () => {
-  //   it('should generate all the products in the db', done => {
-  //     generateProductListForAUnit({});
-  //   }, 15000);
-  // });
-
   it('should be able to create and delete all the given products LESS then 25', done => {
+    // using UNIT 03
     of('CREATE_&_DELETE_LESS_THAN_25_ITEMS')
       .pipe(
         // CREATE
         switchMap(() =>
           createGeneratedProducts({
-            products: [unit01_generatedProduct_01, unit01_generatedProduct_02],
+            products: [unit03_generatedProduct_01, unit03_generatedProduct_02],
           }),
         ),
+        switchMap(() =>
+          listGeneratedProductsForUnits(crudBackendGraphQLClient, [unitId_03]),
+        ),
+        tap({
+          next(result) {
+            expect(getSortedIds(result)).toEqual([
+              unit03_generatedProduct_01.id,
+              unit03_generatedProduct_02.id,
+            ]);
+          },
+        }),
+        // DELETE
+        switchMap(() =>
+          deleteGeneratedProductsForAUnit({
+            unitId: unitId_03,
+            crudGraphqlClient: crudBackendGraphQLClient,
+          }),
+        ),
+        switchMap(() =>
+          listGeneratedProductsForUnits(crudBackendGraphQLClient, [
+            unitId_02,
+            unitId_03,
+          ]),
+        ),
+        tap({
+          next(result) {
+            expect(result.length).toEqual(1);
+            expect(getSortedIds(result)).toEqual([
+              unit02_generatedProduct_01.id,
+            ]);
+          },
+        }),
+      )
+      .subscribe({
+        next() {
+          done();
+        },
+      });
+  }, 25000);
+
+  it('should be able to create and delete MORE then 25 generated products', done => {
+    // using UNIT 01
+    const fullProducts = productIds.map(id => ({
+      ...unit01_generatedProduct_01,
+      id,
+    }));
+
+    expect(productIds).toHaveLength(PRODUCT_NUM_FOR_BATCH_CRUD);
+    expect(fullProducts).toHaveLength(productIds.length);
+
+    of('CREATE_&_DELETE_MORE_THAN_25_ITEMS')
+      .pipe(
+        // CREATE
+        switchMap(() =>
+          createGeneratedProducts({
+            products: fullProducts,
+          }),
+        ),
+        // Count the emission number
+        scan((acc: number) => acc + 1, 0),
+        tap({
+          next(emissionCount) {
+            // The whole buffered batch operation should only emit one result
+            // at the end of the very last db operation
+            expect(emissionCount).toEqual(1);
+          },
+        }),
         switchMap(() =>
           listGeneratedProductsForUnits(crudBackendGraphQLClient, [unitId_01]),
         ),
         tap({
           next(result) {
-            expect(getSortedIds(result)).toEqual([
-              unit01_generatedProduct_01.id,
-              unit01_generatedProduct_02.id,
-            ]);
+            expect(getSortedIds(result)).toEqual(productIds);
+            expect(result).toHaveLength(fullProducts.length);
+            expect(result[0]).toHaveProperty('__typename', 'GeneratedProduct');
           },
         }),
         // DELETE
@@ -101,6 +160,7 @@ describe('GenerateProduct tests', () => {
           ]),
         ),
         tap({
+          // should delete all the generatedProducts for the unit01
           next(result) {
             expect(result.length).toEqual(1);
             expect(getSortedIds(result)).toEqual([
