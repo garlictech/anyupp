9afeac42f0cf1480addd5d363a4994764a853dff
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-04-29 11:15:09 +0200

    build: fix the build with a temporary tableConfig constant

--- libs/crud-gql/backend/src/index.ts
+++ libs/crud-gql/backend/src/index.ts
@@ -1,2 +1,7 @@
-import * as tableConfig from './generated/table-config.json';
-export { tableConfig };
+// TODO: reenable this
+// import * as tableConfig from './generated/table-config.json';
+// export { tableConfig };
+
+export const tableConfig = {
+  GeneratedProduct: 'GeneratedProduct-3vjbjtzr2rhxffhmzbpoo7ozzu-dev',
+};
