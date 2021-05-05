84bfb4e4416e9945c0f4c01b86c0791b63d9d23f
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-05-05 16:56:06 +0200

    feat: add permission to the appsync lambda to handle the generatedProduct table with SDK

--- libs/crud-gql/backend/src/index.ts
+++ libs/crud-gql/backend/src/index.ts
@@ -4,5 +4,10 @@
 
 export const tableConfig = {
   // GeneratedProduct: 'GeneratedProduct-3vjbjtzr2rhxffhmzbpoo7ozzu-dev', // gyurianyupp
-  GeneratedProduct: 'GeneratedProduct-s2zkxdbdf5dujf3krxnt3h5j3i-dev', // dev
+  GeneratedProduct: {
+    tableName: 'GeneratedProduct-s2zkxdbdf5dujf3krxnt3h5j3i-dev', // dev
+    tableArn:
+      'arn:aws:dynamodb:eu-west-1:568276182587:table/GeneratedProduct-s2zkxdbdf5dujf3krxnt3h5j3i-dev', // dev
+    tableStreamArn: '???', // dev
+  },
 };
