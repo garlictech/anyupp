77226309b899a07eed198366f790cab00273596a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-05-23 23:15:44 +0200

    chore(project): wrong gitignore fix

---
+++ libs/anyupp-gql/api/src/lib/clients.ts
@@ -0,0 +1,36 @@
+import {
+  GraphqlApiClient,
+  GraphqlApiFp,
+} from '@bgap/shared/graphql/api-client';
+import { AnyuppSdk, getSdkAnyupp } from './sdk';
+import { pipe } from 'fp-ts/function';
+import { awsConfig } from '@bgap/crud-gql/api';
+import { config } from '@bgap/shared/config';
+
+export const appsyncConfig = {
+  ...awsConfig,
+  aws_appsync_apiKey: config.AnyuppGraphqlApiKey,
+  aws_appsync_graphqlEndpoint: config.AnyuppGraphqlApiUrl,
+};
+
+const createSdk = (gqlClient: GraphqlApiClient) =>
+  (getSdkAnyupp(gqlClient._client) as unknown) as AnyuppSdk;
+
+export const getAnyuppSdkForIAM = (
+  awsAccesskeyId: string,
+  awsSecretAccessKey: string,
+) =>
+  pipe(
+    GraphqlApiFp.createBackendClient(
+      appsyncConfig,
+      awsAccesskeyId,
+      awsSecretAccessKey,
+    ),
+    createSdk,
+  );
+
+export const getAnyuppSdkForUserPool = () =>
+  pipe(GraphqlApiFp.createAuthenticatedClient(appsyncConfig, true), createSdk);
+
+export const getAnyuppSdkPublic = () =>
+  pipe(GraphqlApiFp.createPublicClient(appsyncConfig, true), createSdk);
