be52d22df4acfc25306d173aa639ad11b870604e
Levente <Asztalos>
2021-05-31 11:33:44 +0200

    fix product variant selection

--- apps/anyupp-mobile/lib/modules/menu/widgets/product_configure_widget.dart
+++ apps/anyupp-mobile/lib/modules/menu/widgets/product_configure_widget.dart
@@ -55,7 +55,8 @@ class _ProductConfiguratorWidgetState extends State<ProductConfiguratorWidget> {
           groupValue: _productVariant,
           onChanged: (ProductVariant selectedVariant) {
             setState(() {
-              selectedVariant = _productVariant;
+               _productVariant = selectedVariant;
+              _calculateTotalPrice();
             });
           },
         ),
