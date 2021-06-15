a04b0f265b6578afc762a63b64035da145f176bf
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-06-15 23:44:50 +0200

    fix: update sort logic with the 0 result in case the ids are the same

--- libs/shared/utils/src/lib/fn/graphql.utils.ts
+++ libs/shared/utils/src/lib/fn/graphql.utils.ts
@@ -3,7 +3,7 @@ interface ObjectWithId {
 }
 
 export const getSortedIds = (input: Array<ObjectWithId>) =>
-  input.map(x => x.id).sort((a, b) => (a >= b ? 1 : -1));
+  input.map(x => x.id).sort((a, b) => (a > b ? 1 : a < b ? -1 : 0));
 
 export const sortById = <T extends ObjectWithId>(input: Array<T>): Array<T> =>
-  input.sort((a, b) => (a.id >= b.id ? 1 : -1));
+  input.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
