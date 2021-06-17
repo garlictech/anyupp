7a458fb41f5d4427bdb362eab23f303cc8641345
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-06-18 00:30:08 +0200

    feat: fix delete-admin-user and test

--- libs/integration-tests/universal/src/lib/libs/anyupp-gql/admin-user/create-admin-user.resolver.spec.ts
+++ libs/integration-tests/universal/src/lib/libs/anyupp-gql/admin-user/create-admin-user.resolver.spec.ts
@@ -1,21 +1,51 @@
+import {
+  AdminUserResolverDeps,
+  createAdminUser,
+  deleteAdminUser,
+} from '@bgap/anyupp-gql/backend';
+import { awsConfig } from '@bgap/crud-gql/api';
 import { testAdminEmail, testAdminUserPassword } from '@bgap/shared/fixtures';
-import { of, throwError } from 'rxjs';
+import { pipeDebug } from '@bgap/shared/utils';
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { defer, iif, of, throwError } from 'rxjs';
 import { catchError, switchMap, tap } from 'rxjs/operators';
-import { createAuthenticatedAnyuppSdk } from '../../../../api-clients';
+import {
+  createAuthenticatedAnyuppSdk,
+  createIamCrudSdk,
+} from '../../../../api-clients';
+
+const DEBUG_MODE_TEST_WITH_LOCALE_CODE = true;
+const cognitoidentityserviceprovider = new CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18',
+  region: 'eu-west-1',
+});
+
+const email = 'foobar@anyupp.com';
+const userName = 'intTestUserName';
+const phone = '+1234567892';
+const staticUserNameGenerator = () => userName;
 
 describe('Admin user creation/deletion', () => {
   const authAnyuppSdk = createAuthenticatedAnyuppSdk(
     testAdminEmail,
     testAdminUserPassword,
   );
+  const deps: AdminUserResolverDeps = {
+    userPoolId: awsConfig.aws_user_pools_id,
+    crudSdk: createIamCrudSdk(),
+    cognitoidentityserviceprovider,
+    userNameGenerator: staticUserNameGenerator,
+  };
 
   test('Admin user should be created/deleted', done => {
-    const userName = 'foobar@anyupp.com';
-
     authAnyuppSdk
       .pipe(
         switchMap(({ authAnyuppSdk: sdk }) =>
-          sdk.DeleteAdminUser({ userName }).pipe(
+          iif(
+            () => DEBUG_MODE_TEST_WITH_LOCALE_CODE,
+            defer(() => deleteAdminUser({ userName })(deps)),
+            sdk.DeleteAdminUser({ userName }),
+          ).pipe(
             catchError((err: Error) => {
               console.log('***', err);
               if (err.message.includes('User does not exist')) {
@@ -23,54 +53,90 @@ describe('Admin user creation/deletion', () => {
               }
               return throwError(err);
             }),
-            tap(x => console.log('***', x)),
-            /*switchMap(() =>
-              sdk
-                .CreateAdminUser({
-                  input: {
-                    email: 'foobar',
-                    name: 'Mekk elek',
-                    phone: '12356666',
-                  },
-                })
-                .pipe(
-                  catchError(err => {
-                    expect(err).toMatchSnapshot('Malformed email error');
-                    return of({});
-                  }),
-                ),
-            ),
-                        switchMap(() =>
-              sdk.CreateAdminUser({
-                input: {
-                  email: userName,
-                  name: 'Mekk Elek',
-                  phone: '123456',
-                },
-              }),
-            ),
-            switchMap(() =>
-              sdk
-                .CreateAdminUser({
-                  input: {
-                    email: userName,
-                    name: 'Mekk Elek',
-                    phone: '123456',
-                  },
-                })
-                .pipe(
-                  catchError(err => {
-                    expect(err).toMatchSnapshot(
-                      'Should not create existing user',
-                    );
-                    return of({});
-                  }),
-                ),
-            ),
+            pipeDebug('### AfterDelete'),
+            // ERROR - Invalid phoneNumber
+            switchMap(() => {
+              const input = {
+                email: 'NOT_VALID_EMAIL',
+                name: 'Mekk elek',
+                phone: 'NOT_VALID_PHONE_NUMBER',
+              };
+              return iif(
+                () => DEBUG_MODE_TEST_WITH_LOCALE_CODE,
+                defer(() => createAdminUser({ input })(deps)),
+                sdk.CreateAdminUser({ input }),
+              ).pipe(
+                catchError(err => {
+                  expect(err).toMatchSnapshot('Invalid phone number error');
+                  return of({});
+                }),
+              );
+            }),
+            // ERROR - Invalid mail
+            switchMap(() => {
+              const input = {
+                email: 'NOT_VALID_EMAIL',
+                name: 'Mekk elek',
+                phone,
+              };
+              return iif(
+                () => DEBUG_MODE_TEST_WITH_LOCALE_CODE,
+                defer(() => createAdminUser({ input })(deps)),
+                sdk.CreateAdminUser({ input }),
+              ).pipe(
+                catchError(err => {
+                  expect(err).toMatchSnapshot('Malformed email error');
+                  return of({});
+                }),
+              );
+            }),
+            // SUCCESSFULL CREATE
+            switchMap(() => {
+              const input = {
+                email,
+                name: 'Mekk Elek',
+                phone,
+              };
+              return iif(
+                () => DEBUG_MODE_TEST_WITH_LOCALE_CODE,
+                defer(() => createAdminUser({ input })(deps)),
+                sdk.CreateAdminUser({ input }),
+              ).pipe(
+                catchError(err => {
+                  console.error('SHOULD NOT THROW', err);
+                  return throwError(err);
+                }),
+              );
+            }),
+            // ERROR - Existing user
+            switchMap(() => {
+              const input = {
+                email,
+                name: 'Mekk Elek',
+                phone,
+              };
+              return iif(
+                () => DEBUG_MODE_TEST_WITH_LOCALE_CODE,
+                defer(() => createAdminUser({ input })(deps)),
+                sdk.CreateAdminUser({ input }),
+              ).pipe(
+                catchError(err => {
+                  expect(err).toMatchSnapshot(
+                    'Should not create existing user',
+                  );
+                  return of({});
+                }),
+              );
+            }),
             // Cleanup
-            switchMap(() => sdk.DeleteAdminUser({ userName })),
+            switchMap(() =>
+              iif(
+                () => DEBUG_MODE_TEST_WITH_LOCALE_CODE,
+                defer(() => deleteAdminUser({ userName })(deps)),
+                sdk.DeleteAdminUser({ userName }),
+              ),
+            ),
             tap(result => expect(result).toMatchSnapshot('Cleanup')),
-        */
           ),
         ),
       )
