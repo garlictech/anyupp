73bd2bd288bddfd81a144755ab80007fac473d5e
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-06-22 13:48:02 +0200

    chore(tools): ios appcenter submission fix for prod

--- tools/trigger-appcenter-builds.sh
+++ tools/trigger-appcenter-builds.sh
@@ -5,6 +5,8 @@ IFS='|'
 STAGE=$1
 PLATFORM=$2
 
+branch=$STAGE
+
 if [ $STAGE = 'dev'  ] && [ $PLATFORM = 'ios' ]; then
   appId="3FA"
 elif [ $STAGE = 'qa'  ] && [ $PLATFORM = 'ios' ]; then
@@ -19,13 +21,15 @@ elif [ $STAGE = 'staging'  ] && [ $PLATFORM = 'android' ]; then
   appId="AnyUpp-Android-Staging"
 elif [ $STAGE = 'prod'  ] && [ $PLATFORM = 'android' ]; then
   appId="AnyUpp-Android-PROD"
+  branch="master"
 elif [ $STAGE = 'prod'  ] && [ $PLATFORM = 'ios' ]; then
   appId="AnyUpp-iOS-PROD"
+  branch="master"
 else
   echo "Unsupported app: ${STAGE}/${PLATFORM}"
   exit 1
 fi
 
 echo "Trigger app build in stage ${STAGE}, platform ${PLATFORM}"
-appcenter build queue -b ${STAGE} -a "Bitgap/${appId}" --token $APP_CENTER_TOKEN
+appcenter build queue -b ${branch} -a "Bitgap/${appId}" --token $APP_CENTER_TOKEN
 
