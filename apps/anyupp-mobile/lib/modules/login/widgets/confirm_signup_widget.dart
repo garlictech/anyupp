4f8fec0954093491ad9f8bd72a28871754f8c129
Levente <Asztalos>
2021-06-28 14:50:40 +0200

    added user pool domains

--- apps/anyupp-mobile/lib/modules/login/widgets/confirm_signup_widget.dart
+++ apps/anyupp-mobile/lib/modules/login/widgets/confirm_signup_widget.dart
@@ -13,7 +13,6 @@ class ConfirmSignUpWidget extends StatefulWidget {
 
 class _ConfirmSignUpWidgetState extends State<ConfirmSignUpWidget> {
   final _confirmCodeController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
   String userName;
 
   @override
@@ -34,10 +33,9 @@ class _ConfirmSignUpWidgetState extends State<ConfirmSignUpWidget> {
                 'login.email.checkInbox',
               ));
 
-          return _buildSignUpConfirmForm(context, state.user);
+          return _buildConfirmLinkSent(context, state.user);
         }
 
-        // TODO: implement listener
       },
       child: BlocBuilder<LoginBloc, LoginState>(
         builder: (BuildContext context, LoginState state) {
@@ -77,136 +75,6 @@ class _ConfirmSignUpWidgetState extends State<ConfirmSignUpWidget> {
             ),
           )
         ],
-      ),
-    );
-  }
-
-  Widget _buildSignUpConfirmForm(BuildContext context, String user) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Stack(
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 8.0, left: 12.0, right: 12.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: trans('login.email.setMailTo') + ' ',
-                              style: GoogleFonts.poppins(
-                                fontSize: 14.0,
-                                color: Color(0x993C2F2F),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    // Email + passwords input fields
-                    LoginFormUtils.buildTextField(
-                        context,
-                        trans('login.email.verificationCode'),
-                        _confirmCodeController,
-                        TextInputType.emailAddress,
-                        false,
-                        LoginFormUtils.confirmCodeValidator(context)),
-
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          RichText(
-                            textAlign: TextAlign.center,
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: trans('login.email.codeNotReceived') +
-                                      ' ',
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 14.0,
-                                    color: Color(0x993C2F2F),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () async {
-                              _resendCode(user);
-                            },
-                            child: Text(
-                              trans('login.email.resendCode'),
-                              textAlign: TextAlign.start,
-                              style: GoogleFonts.poppins(
-                                fontSize: 14,
-                                color: theme.highlight,
-                                fontWeight: FontWeight.normal,
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Sing in link button
-                    InkWell(
-                      onTap: () {
-                        getIt<LoginBloc>().add(ChangeEmailFormUI(
-                            ui: LoginFormUI.SHOW_LOGIN_WITH_PASSWORD,
-                            animationCurve: Curves.easeIn));
-                      },
-                      child: Text(
-                        trans('login.email.linkSignIn'),
-                        textAlign: TextAlign.start,
-                        style: GoogleFonts.poppins(
-                          fontSize: 14,
-                          color: theme.highlight,
-                          fontWeight: FontWeight.normal,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-
-                    // Buttons
-                    SizedBox(
-                      width: double.infinity,
-                      height: 52.0,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Color(0xFF30BF60),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                        child: Text(
-                          trans('login.email.confirmAccount'),
-                          style: GoogleFonts.poppins(
-                            color: theme.text2,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                        onPressed: () => _confirmSigInCode(user),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
@@ -374,15 +242,6 @@ class _ConfirmSignUpWidgetState extends State<ConfirmSignUpWidget> {
         ),
       ],
     );
-  }
-
-  void _confirmSigInCode(String user) {
-    print('_confirmSigInCode()=${_confirmCodeController.text}');
-    if (_formKey.currentState.validate()) {
-      _formKey.currentState.save();
-      getIt<LoginBloc>()
-          .add(ConfirmRegistration(user, _confirmCodeController.text));
-    }
   }
 
   void _resendCode(String user) {
