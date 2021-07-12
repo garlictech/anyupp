515c0a87b9341726c713f6f7abfcac2c42754c5b
Tamás Petró <petro.tamas@gmail.com>
2021-07-12 17:28:45 +0200

    feat(admin): get mailtrap api key from the parameter store

--- apps/admin-e2e/src/integration/auth/auth/mail.steps.ts
+++ apps/admin-e2e/src/integration/auth/auth/mail.steps.ts
@@ -1,26 +1,26 @@
+import { mailtrapBox } from 'apps/admin-e2e/src/support/mailtrap';
 import { When } from 'cypress-cucumber-preprocessor/steps';
-import { Client, Inbox } from 'mailtrap';
 
-const MAILTRAP_API_TOKEN = '646cceec28fd28c1ed832b13e48ad2d1';
-const client = new Client({
-  apiToken: MAILTRAP_API_TOKEN,
-});
-const inbox = new Inbox(client, 'Dev');
+const mtBox = mailtrapBox();
 
 When('I delete all messages from the inbox', async () => {
-  await inbox.deleteMessages();
+  await mtBox.inbox.deleteMessages();
 });
 
 When('I wait for the message', async () => {
-  await inbox.waitForMessages(messages => messages.length > 0);
+  await mtBox.inbox.waitForMessages(messages => messages.length > 0);
 });
 
 When('I read and type the verification code from the email', async () => {
-  const messages = await inbox.getMessages();
+  const messages = await mtBox.inbox.getMessages();
   const msgID = messages[0]?.id;
 
   if (msgID) {
-    const txt = await client.getMessageBody(inbox.ID, msgID, 'html');
+    const txt = await mtBox.client.getMessageBody(
+      mtBox.inbox.ID,
+      msgID,
+      'html',
+    );
     // Get the last word from the body "The verification code to your new account is 580684"
     const code = txt.split(' ').pop();
 
