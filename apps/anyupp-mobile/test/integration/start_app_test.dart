2020fa1fb5d18b3d1d8eab473ac4ce5b1ee09c38
admin <admin@ihu92.local>
2021-07-13 17:54:23 +0200

    chore: merge dev

--- apps/anyupp-mobile/test/integration/start_app_test.dart
+++ apps/anyupp-mobile/test/integration/start_app_test.dart
@@ -1,13 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:fa_prev/main.dart' as app;
 
-void main() {
+Future<void> pumpUntilFound(
+  WidgetTester tester,
+  Finder finder, {
+  Duration timeout = const Duration(seconds: 50),
+}) async {
+  bool timerDone = false;
+  final timer =
+      Timer(timeout, () => throw TimeoutException("Pump until has timed out"));
+  while (timerDone != true) {
+    await tester.pump();
 
+    final found = tester.any(finder);
+    if (found) {
+      timerDone = true;
+    }
+  }
+  timer.cancel();
+}
+
+void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  
   group('AnyUpp Mobile App', () {
+    // setUpAll(() async {
+    //   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    // });
     // First, define the Finders and use them to locate widgets from the
     // test suite. Note: the Strings provided to the `byValueKey` method must
     // be the same as the Strings we used for the Keys in step 1.
@@ -17,33 +39,52 @@ void main() {
     // final buttonSubtract = find.byValueKey("subtract");
     // final alertText = find.byValueKey("alert_text");
     // final btnClose = find.byValueKey("close_button");
-    // final Finder main = find.byKey(Key('anyupp-main-app')); 
+    // final Finder main = find.byKey(Key('anyupp-main-app'));
     // final Function(FlutterErrorDetails) original = FlutterError.onError;
-
-
     testWidgets('Checks the app is started', (WidgetTester tester) async {
-      // // First, tap on the button
-      // await driver.tap(buttonFinder);
-
       print('****** Waiting the app to start');
       app.main();
-      await tester.pumpAndSettle(Duration(seconds: 10));
       print('****** App started');
 
-      // await Future.delayed(Duration(seconds: 10));
-      print('****** Restoring flutter onError');
-      // FlutterError.onError = original;
-
       final Finder main = find.byKey(Key('anyupp-main-app'));
-      print('****** MAIN = ${main.description}');
-      await tester.pump();
-
+      await pumpUntilFound(tester, main);
+      //print('****** MAIN = ${main.description}');
       expect(main, isNotNull);
-    });
 
-    testWidgets("test login with Email", (WidgetTester tester) async {
+      final Finder loginBtn = find.byKey(Key('login-btn-anonymous'));
+      //print('****** loginBtn = ${loginBtn.description}');
 
-      expect(true, equals(true));
+      await pumpUntilFound(tester, loginBtn);
+      await tester.pumpAndSettle();
+
+      expect(loginBtn, isNotNull);
+
+      await tester.tap(loginBtn);
+      final Finder unitSelectSreen = find.byKey(Key('unitselect-screen'));
+      await pumpUntilFound(tester, unitSelectSreen);
+      expect(unitSelectSreen, isNotNull);
+
+      // final Finder unitWidget = find.byKey(Key('unit-widget'));
+      // //await pumpUntilFound(tester, unitWidget);
+      // await tester.pumpAndSettle();
+      // expect(unitWidget, isNotNull);
+
+      //  await tester.tap(unitWidget);
+      
     });
+    // testWidgets("test login with Anonymously", (WidgetTester tester) async {
+    //   await tester.pumpAndSettle(Duration(seconds: 15));
+    //   final Finder loginBtn = find.byKey(Key('login-btn-anonymous'));
+    //   print('****** loginBtn = ${loginBtn.description}');
+    //   expect(loginBtn, isNotNull);
+    //   await tester.tap(find.byKey(Key('login-btn-anonymous')));
+    //   await tester.pumpAndSettle();
+    // });
+    // testWidgets("checks that we are on the Unit Selection screen", (WidgetTester tester) async {
+    //   await tester.pump(Duration(seconds: 5));
+    //   final Finder unitSelectSreen = find.byKey(Key('unitselect-screen'));
+    //   // print('****** loginBtn = ${loginBtn.description}');
+    //   expect(unitSelectSreen, isNotNull);
+    // });
   });
 }
