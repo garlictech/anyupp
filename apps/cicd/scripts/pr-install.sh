4e7690d42748a9df35f3768013823b91d3c2d2fa
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-07-13 23:43:47 +0200

    chore(ci): pr execution optimizations

--- apps/cicd/scripts/pr-install.sh
+++ apps/cicd/scripts/pr-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-time ./tools/install-nodejs-14.sh &
+time ./tools/install-nodejs-14.sh 
 nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 &
 timeout 15 sh -c "until docker info; do echo .; sleep 1; done"
 docker pull cirrusci/flutter &
 chmod +x ./tools/*.sh
-./tools/setup-aws-environment.sh &
-time npm install -g @aws-amplify/cli cowsay &
-time yarn --frozen-lockfile &
+./tools/setup-aws-environment.sh 
+time npm install -g @aws-amplify/cli cowsay 
+time yarn --frozen-lockfile 
 npx cowsay "STARTING PR CHECK"
