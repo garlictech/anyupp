f768672b7de22fc0e3e4d4cbf1dca29988e09943
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-07-13 21:59:10 +0200

    chore(ci): start docker daemon

--- apps/cicd/scripts/pr-install.sh
+++ apps/cicd/scripts/pr-install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+time ./tools/install-nodejs-14.sh &
+nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 &
+timeout 15 sh -c "until docker info; do echo .; sleep 1; done"
 docker pull cirrusci/flutter 
 chmod +x ./tools/*.sh
 ./tools/setup-aws-environment.sh
-time ./tools/install-nodejs-14.sh
 time yarn --frozen-lockfile
 time npm install -g @aws-amplify/cli cowsay
-npx cowsay "STARTING PR CHECK"',
+npx cowsay "STARTING PR CHECK"
