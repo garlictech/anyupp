2a325d1af62919fe2691accfbbfbb78481c58980
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-07-14 14:47:13 +0200

    chore(ci): pr execution optimizations

--- apps/cicd/scripts/pr-build.sh
+++ apps/cicd/scripts/pr-build.sh
@@ -7,10 +7,10 @@ STAGE=$2
 generatedLibExcludes="--exclude=shared-config --exclude=anyupp-gql-api --exclude=crud-gql-api"
 
 time ./tools/build-workspace.sh ${APPNAME} ${STAGE}
-#time yarn nx format:check --affected
-#time yarn nx affected:lint --base=${STAGE} ${generatedLibExcludes} 
-#time yarn nx affected:test --base=${STAGE} --exclude="integration-tests-angular" --exclude="integration-tests-universal" ${generatedLibExcludes} --codeCoverage --coverageReporters=clover
-#time yarn nx affected:test --base=${STAGE} --exclude="integration-tests-angular" --exclude="integration-tests-universal" --exclude="anyupp-mobile" ${generatedLibExcludes} --codeCoverage --coverageReporters=clover
+time yarn nx format:check --affected
+time yarn nx affected:lint --base=${STAGE} ${generatedLibExcludes} 
+time yarn nx affected:test --base=${STAGE} --exclude="integration-tests-angular" --exclude="integration-tests-universal" ${generatedLibExcludes} --codeCoverage --coverageReporters=clover
+time yarn nx affected:test --base=${STAGE} --exclude="integration-tests-angular" --exclude="integration-tests-universal" --exclude="anyupp-mobile" ${generatedLibExcludes} --codeCoverage --coverageReporters=clover
 time yarn nx lint anyupp-mobile 
 time yarn nx test-ci anyupp-mobile 
 time yarn nx buildApk-ci anyupp-mobile
