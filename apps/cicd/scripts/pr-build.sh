c81fde5797b3b7ce1112a5c89e799a291100b27a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-07-14 14:04:41 +0200

    chore(ci): pr execution optimizations

--- apps/cicd/scripts/pr-build.sh
+++ apps/cicd/scripts/pr-build.sh
@@ -6,7 +6,7 @@ STAGE=$2
 
 generatedLibExcludes="--exclude=shared-config --exclude=anyupp-gql-api --exclude=crud-gql-api"
 
-#time ./tools/build-workspace.sh ${APPNAME} ${STAGE}
+time ./tools/build-workspace.sh ${APPNAME} ${STAGE}
 #time yarn nx format:check --affected
 #time yarn nx affected:lint --base=${STAGE} ${generatedLibExcludes} 
 #time yarn nx affected:test --base=${STAGE} --exclude="integration-tests-angular" --exclude="integration-tests-universal" ${generatedLibExcludes} --codeCoverage --coverageReporters=clover
