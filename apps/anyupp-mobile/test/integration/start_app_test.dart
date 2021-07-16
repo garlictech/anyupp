b23d6e932c65d5611a0a7cfef593d455b750b92e
admin <admin@ihu92.local>
2021-07-16 13:36:26 +0200

    chore: back to initial e2e test

--- apps/anyupp-mobile/test/integration/start_app_test.dart
+++ apps/anyupp-mobile/test/integration/start_app_test.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:fa_prev/main.dart' as app;
+
 void main() {
+
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  
   group('AnyUpp Mobile App', () {
-    // setUpAll(() async {
-    //   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-    // });
     // First, define the Finders and use them to locate widgets from the
     // test suite. Note: the Strings provided to the `byValueKey` method must
     // be the same as the Strings we used for the Keys in step 1.
@@ -17,45 +17,33 @@ void main() {
     // final buttonSubtract = find.byValueKey("subtract");
     // final alertText = find.byValueKey("alert_text");
     // final btnClose = find.byValueKey("close_button");
-    // final Finder main = find.byKey(Key('anyupp-main-app'));
+    // final Finder main = find.byKey(Key('anyupp-main-app')); 
     // final Function(FlutterErrorDetails) original = FlutterError.onError;
+
+
     testWidgets('Checks the app is started', (WidgetTester tester) async {
       // // First, tap on the button
       // await driver.tap(buttonFinder);
+
       print('****** Waiting the app to start');
       app.main();
-      await tester.pumpAndSettle(Duration(seconds: 5));
+      await tester.pumpAndSettle(Duration(seconds: 10));
       print('****** App started');
+
       // await Future.delayed(Duration(seconds: 10));
+      print('****** Restoring flutter onError');
+      // FlutterError.onError = original;
+
       final Finder main = find.byKey(Key('anyupp-main-app'));
-      // print('****** MAIN = ${main.description}');
-      // await tester.pumpAndSettle();
+      print('****** MAIN = ${main.description}');
+      await tester.pump();
+
       expect(main, isNotNull);
-      await tester.pumpAndSettle(Duration(seconds: 20));
-      // await Future.delayed(Duration(seconds: 15));
-      final Finder loginBtn = find.byKey(Key('login-btn-anonymous'));
-      print('****** loginBtn = ${loginBtn.description}');
-      expect(loginBtn, isNotNull);
-      await tester.tap(find.byKey(Key('login-btn-anonymous')));
-      // await tester.pumpAndSettle();
-      await tester.pump(Duration(seconds: 15));
-      final Finder unitSelectSreen = find.byKey(Key('unitselect-screen'));
-      // print('****** loginBtn = ${loginBtn.description}');
-      expect(unitSelectSreen, isNotNull);
     });
-    // testWidgets("test login with Anonymously", (WidgetTester tester) async {
-    //   await tester.pumpAndSettle(Duration(seconds: 15));
-    //   final Finder loginBtn = find.byKey(Key('login-btn-anonymous'));
-    //   print('****** loginBtn = ${loginBtn.description}');
-    //   expect(loginBtn, isNotNull);
-    //   await tester.tap(find.byKey(Key('login-btn-anonymous')));
-    //   await tester.pumpAndSettle();
-    // });
-    // testWidgets("checks that we are on the Unit Selection screen", (WidgetTester tester) async {
-    //   await tester.pump(Duration(seconds: 5));
-    //   final Finder unitSelectSreen = find.byKey(Key('unitselect-screen'));
-    //   // print('****** loginBtn = ${loginBtn.description}');
-    //   expect(unitSelectSreen, isNotNull);
-    // });
+
+    testWidgets("test login with Email", (WidgetTester tester) async {
+
+      expect(true, equals(true));
+    });
   });
 }
