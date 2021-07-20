ce9f3159123d1d920c942273776761a843af53fe
zsapkagy <gyorgy.zsapka@gmail.com>
2021-07-20 14:42:30 +0200

    feat: add better validation rules to dateInterval and weeklySchedule (#1216)
    * feat: add better validation rules to dateInterval and weeklySchedule
    
    * chore: fix lint errors

--- libs/shared/data-validators/src/lib/interfaces/unit.ts
+++ libs/shared/data-validators/src/lib/interfaces/unit.ts
@@ -1,10 +1,9 @@
-import * as Joi from 'joi';
-
 import * as CrudApi from '@bgap/crud-gql/api';
-
+import * as Joi from 'joi';
 import { validateGqlList, validateSchema } from '../validator/validate';
 import { addressInfoSchema } from './address';
 import { contactSchema } from './contact';
+import { dateStringSchema, timeStringSchema } from './date-time';
 import { floorMapSchema } from './floor-map';
 import { localizedItemSchema } from './localized-item';
 import { paymentModeSchema } from './payment';
@@ -15,6 +14,37 @@ export const laneSchema: Joi.SchemaMap<CrudApi.Lane> = {
   color: Joi.string().required(),
 };
 
+const dateIntervalOpenSchema = Joi.object({
+  from: dateStringSchema.allow(null, ''),
+  to: dateStringSchema.allow(null, ''),
+}).or('from', 'to');
+const timeIntervalSchema = Joi.alternatives(
+  Joi.object({
+    from: timeStringSchema.required(),
+    to: timeStringSchema.required(),
+  }),
+  Joi.object({
+    from: Joi.string().required().valid(''),
+    to: Joi.string().required().valid(''),
+  }),
+  Joi.any().valid(null),
+);
+const customDailyScheduleSchema = Joi.object({
+  date: dateStringSchema.required(),
+  from: timeStringSchema.allow(null, ''),
+  to: timeStringSchema.allow(null, ''),
+});
+const weeklySchedule = Joi.object({
+  mon: timeIntervalSchema,
+  tue: timeIntervalSchema,
+  wed: timeIntervalSchema,
+  thu: timeIntervalSchema,
+  fri: timeIntervalSchema,
+  sat: timeIntervalSchema,
+  sun: timeIntervalSchema,
+  custom: Joi.array().items(customDailyScheduleSchema).allow(null),
+});
+
 export const unitSchema: Joi.SchemaMap<CrudApi.Unit> = {
   id: Joi.string().required(),
   groupId: Joi.string().required(),
@@ -23,8 +53,8 @@ export const unitSchema: Joi.SchemaMap<CrudApi.Unit> = {
   isAcceptingOrders: Joi.boolean().required(),
   name: Joi.string().required(),
   description: localizedItemSchema.required(),
-  open: Joi.object(),
-  openingHours: Joi.object().allow(null),
+  open: dateIntervalOpenSchema.allow(null),
+  openingHours: weeklySchedule.allow(null),
   lanes: Joi.array().items(laneSchema).allow(null),
   floorMap: Joi.object(floorMapSchema).allow(null),
   lastOrderNum: Joi.number().allow(null),
