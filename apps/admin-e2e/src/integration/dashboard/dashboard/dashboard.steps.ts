ad5002362d1dfae5ecb9257332e3c0cb6a025dca
Tamás Petró <petro.tamas@gmail.com>
2021-07-20 15:29:19 +0200

    fix(admin): e2e lint fix

--- apps/admin-e2e/src/integration/dashboard/dashboard/dashboard.steps.ts
+++ apps/admin-e2e/src/integration/dashboard/dashboard/dashboard.steps.ts
@@ -1,16 +1,26 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
 
-When('I create a cash order', () => {});
-When('I set order status to {string}', (status: string) => {});
+When('I create a cash order', () => {
+  /**/
+});
+When('I set order status to {string}', (/*status: string*/) => {
+  /**/
+});
 When(
   'I should see {string} seat with {string} id on the floormap',
-  (bgColor: string, seatId: string) => {},
+  (/*bgColor: string, seatId: string*/) => {
+    /**/
+  },
 );
 When(
   'I should see {string} border with {string} id on the floormap',
-  (borderColor: string, seatId: string) => {},
+  (/*borderColor: string, seatId: string*/) => {
+    /**/
+  },
 );
 When(
   'I set order transaction status to {string}',
-  (transactionStatus: string) => {},
+  (/*transactionStatus: string*/) => {
+    /**/
+  },
 );
