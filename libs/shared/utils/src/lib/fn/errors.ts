f0e6a285446eca9970f538a7ff26da5afb7f85fe
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-07-21 11:33:00 +0200

    feat: use better error message in case unit not accepting orders

--- libs/shared/utils/src/lib/fn/errors.ts
+++ libs/shared/utils/src/lib/fn/errors.ts
@@ -2,7 +2,10 @@ export const missingParametersError = (paramName: string) =>
   `${paramName} parameter is missing`;
 
 export const getUnitIsNotAcceptingOrdersError = () =>
-  'Unit is not accepting orders right now. Try it later!';
+  JSON.stringify({
+    message: 'Unit is not accepting orders right now. Try it later!',
+    code: 'ERROR_UNIT_NOT_ACCEPTING_ORDERS',
+  });
 
 export const getCartIsMissingError = () => 'Cart is missing';
 
