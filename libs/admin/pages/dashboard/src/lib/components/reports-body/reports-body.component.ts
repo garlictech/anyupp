49bf42316bfac823831edbca6962c45a17e9c312
Tamás Petró <petro.tamas@gmail.com>
2021-07-25 20:42:27 +0200

    fix(admin): lint fix

--- libs/admin/pages/dashboard/src/lib/components/reports-body/reports-body.component.ts
+++ libs/admin/pages/dashboard/src/lib/components/reports-body/reports-body.component.ts
@@ -1,7 +1,3 @@
-import {
-  dashboardActions,
-  dashboardSelectors,
-} from 'libs/admin/shared/data-access/dashboard/src';
 import { BehaviorSubject } from 'rxjs';
 import { skipWhile, take } from 'rxjs/operators';
 
@@ -13,14 +9,18 @@ import {
   OnInit,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {
+  dashboardActions,
+  dashboardSelectors,
+} from '@bgap/admin/shared/data-access/dashboard';
 import { groupsSelectors } from '@bgap/admin/shared/data-access/groups';
 import { ordersSelectors } from '@bgap/admin/shared/data-access/orders';
 import * as CrudApi from '@bgap/crud-gql/api';
 import { IKeyValueObject } from '@bgap/shared/types';
 import {
   filterNullish,
-  orderHasIncome,
   isRejectedOrder,
+  orderHasIncome,
 } from '@bgap/shared/utils';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { select, Store } from '@ngrx/store';
