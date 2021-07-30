ba44677e58f607a6820c80b5041f34f3c73e4ef9
Lau-nagy <73823971+Lau-nagy@users.noreply.github.com>
2021-07-30 11:35:06 +0200

    Nl admin fix product and component tests (#1340)
    * test(e2e): test on list position
    
    * test(e2e): fix product and component tests
    
    * test(e2e): small fix
    
    * test(e2e): mini fix
    
    Co-authored-by: Laura <matuz.lau@gmail.com>
    Co-authored-by: Zsolt R. Molnar <zsolt@zsoltmolnar.hu>

--- apps/admin-e2e/src/integration/product/product-unit/product-unit.steps.ts
+++
@@ -1,9 +0,0 @@
-import { When } from 'cypress-cucumber-preprocessor/steps';
-
-When(
-  'I click the {string} day selector to set {string}',
-  (name: string, value: string) => {
-    cy.findByTestId(name).click();
-    cy.get('nb-option').contains(value).click();
-  },
-);
--- apps/admin-e2e/src/support/utils.ts
+++ apps/admin-e2e/src/support/utils.ts
@@ -1,5 +1,7 @@
-export const getActiveTab = () => cy.get('nb-tab[ng-reflect-active="true"]');
-export const getListItemFromActiveTabThatContains = (value: string) =>
-  getActiveTab().contains('nb-list-item', value).should('be.visible');
+export const getActiveTab = () => cy.get('nb-tab.content-active');
+
 export const getListItemThatContains = (value: string) =>
   cy.contains('nb-list-item', value).should('be.visible');
+
+export const getListItemFromActiveTabThatContains = (value: string) =>
+  getActiveTab().contains('nb-list-item', value).should('be.visible');
