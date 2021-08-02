0e043de69a25585bb36a426b4ea12e9c4adbaff4
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-08-02 18:46:25 +0200

    fix(anyupp-backend): test user email fixes

--- libs/shared/fixtures/src/lib/user.ts
+++ libs/shared/fixtures/src/lib/user.ts
@@ -1,11 +1,11 @@
-export const testAdminEmail = 'test@anyupp.com';
+export const testAdminUsername = 'test';
 export const testAdminUserPassword = 'Hideghegy12_';
 
-export const otherAdminEmails = [
-  'test-bob@anyupp.com',
-  'test-alice@anyupp.com',
-  'test-monad@anyupp.com',
-  'test-kati@anyupp.com',
-  'test-emen@anyupp.com',
-  'test-e2e@anyupp.com',
+export const otherAdminUsernames = [
+  'bob',
+  'alice',
+  'monad',
+  'kati',
+  'emen',
+  'e2e',
 ];
