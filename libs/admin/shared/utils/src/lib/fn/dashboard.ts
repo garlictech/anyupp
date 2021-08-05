bde7d09a095593777c2b0cf1dd92618bc5a42058
Tamás Petró <petro.tamas@gmail.com>
2021-08-05 12:46:43 +0200

    ordering fixes (#1383)
    * fix(admin): fix failed orders filtering
    
    * feat(admin): order recall from history
    
    * feat(admin): dashboard e2e test scenarios
    
    * feat(admin): order recall role handling
    
    * feat(admin): admin util test
    
    * feat(admin): add unpayCategory to the Order schema
    
    * fix(admin): e2e lint fix
    
    * feat(admin): unpay-categories wip
    
    * feat(admin): unpay category select
    
    * fix(admin): schema updates, manual test descriptions
    
    * fix(admin): missing postalCode to print preview
    
    * fix(admin): lint fix
    
    * fix(admin): lint fix
    
    * fix(admin): validator fixes
    
    * feat(admin): unpay stats
    
    * fix(admin): lint fix
    
    * fix(admin): lint fix
    
    * feat(admin): report pure fn refactor
    
    * fix(admin): orders ordering fix
    
    * fix(admin): temp - disable mobile test
    
    Co-authored-by: Zsolt R. Molnar <zsolt@zsoltmolnar.hu>

--- libs/admin/shared/utils/src/lib/fn/dashboard.ts
+++ libs/admin/shared/utils/src/lib/fn/dashboard.ts
@@ -1,5 +1,11 @@
 import * as CrudApi from '@bgap/crud-gql/api';
-import { UnpayCategoryStatObjItem } from '@bgap/shared/types';
+import {
+  EProductType,
+  IKeyValueObject,
+  IOrderAmount,
+  IOrderAmounts,
+  UnpayCategoryStatObjItem,
+} from '@bgap/shared/types';
 
 export const calculateUnpayCategoryStat = (
   category: CrudApi.UnpayCategory,
@@ -14,4 +20,92 @@ export const calculateUnpayCategoryStat = (
     ),
     uniqueUsersCount: [...new Set(categoryOrders.map(o => o.userId))].length,
   };
+};
+
+export const getDailyOrdersSum = (
+  dailyHistoryOrders: CrudApi.Order[],
+): number => {
+  let sum = 0;
+
+  dailyHistoryOrders.forEach(o => {
+    sum += o.sumPriceShown.priceSum;
+  });
+
+  return sum;
+};
+
+export const hourlyBreakdownOrderAmounts = (
+  products: CrudApi.GeneratedProduct[],
+  orders: CrudApi.Order[],
+): IOrderAmount => {
+  const amounts: IOrderAmount = {
+    [EProductType.DRINK]: new Array(24).fill(0),
+    [EProductType.FOOD]: new Array(24).fill(0),
+    [EProductType.OTHER]: new Array(24).fill(0),
+    ordersCount: new Array(24).fill(0),
+    sum: new Array(24).fill(0),
+  };
+
+  const productTypeMap: IKeyValueObject = {};
+  products
+    .filter(p => !!p.id)
+    .forEach(p => {
+      productTypeMap[p.id] = p.productType;
+    });
+
+  orders.forEach(o => {
+    const hour = new Date(o.createdAt || 0).getHours();
+    o.items
+      ?.filter(i => productTypeMap[i.productId])
+      .forEach((i: CrudApi.OrderItem) => {
+        amounts[<EProductType>productTypeMap[i.productId]][hour] +=
+          i.priceShown.priceSum;
+        amounts['sum'][hour] += i.priceShown.priceSum;
+      });
+
+    amounts['ordersCount'][hour] += 1;
+  });
+
+  return amounts;
+};
+
+export const dailySalesPerTypeOrderAmounts = (
+  products: CrudApi.GeneratedProduct[],
+  orders: CrudApi.Order[],
+) => {
+  const amounts: IOrderAmounts = {
+    [EProductType.DRINK]: 0,
+    [EProductType.FOOD]: 0,
+    [EProductType.OTHER]: 0,
+  };
+
+  const productTypeMap: IKeyValueObject = {};
+  products.forEach(p => {
+    productTypeMap[p.id] = p.productType;
+  });
+
+  orders.forEach(o => {
+    o.items.forEach(i => {
+      amounts[<EProductType>productTypeMap[i.productId]] +=
+        i.priceShown.priceSum;
+    });
+  });
+
+  return amounts;
+};
+
+export const dailySalesPerPaymentMethodOrderAmounts = (
+  orders: CrudApi.Order[],
+) => {
+  const amounts: IOrderAmounts = {
+    [CrudApi.PaymentMethod.card]: 0,
+    [CrudApi.PaymentMethod.cash]: 0,
+    [CrudApi.PaymentMethod.inapp]: 0,
+  };
+
+  orders.forEach(o => {
+    amounts[o.paymentMode.method] += o.sumPriceShown.priceSum;
+  });
+
+  return amounts;
 };
