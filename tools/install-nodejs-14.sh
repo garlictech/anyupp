087eb39bd6138478dba016843d8359d9ddf8007d
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-08-10 17:03:48 +0200

    fix(tools): node14 install fixed

--- tools/install-nodejs-14.sh
+++ tools/install-nodejs-14.sh
@@ -6,8 +6,11 @@ package_name="node-$version-$distro"
 package_location="/usr/local/lib/"
 
 curl -O https://nodejs.org/download/release/$version/$package_name.tar.gz
-tar -xvf $package_name.tar.gz -C $package_location
-rm -rfv $package_name.tar.gz
+tar -xf $package_name.tar.gz -C $package_location
+rm -rf $package_name.tar.gz
 
 ln -sf $package_location/$package_name/bin/node `which node`
 
+echo "export PATH=$package_location/$package_name/bin:\$PATH" >> ~/.profile
+echo "export PATH=$package_location/$package_name/bin:\$PATH" >> ~/.bashrc
+export PATH=$package_location/$package_name/bin:\$PATH
