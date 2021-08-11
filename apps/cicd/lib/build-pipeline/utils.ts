3101353a98fa5b024dfdda10929e14b9afed722b
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-08-11 15:39:01 +0200

    fix(tools): node14 install fix

--- apps/cicd/lib/build-pipeline/utils.ts
+++ apps/cicd/lib/build-pipeline/utils.ts
@@ -319,24 +319,16 @@ export const createCommonDevPipeline = (
     },
     buildProjectPhases: {
       install: {
-        commands: [
-          'chmod +x ./tools/*.sh',
-          `./tools/setup-aws-environment.sh`,
-          'yarn --frozen-lockfile',
-          'npm install -g @aws-amplify/cli appcenter-cli',
-        ],
+        commands: ['apps/cicd/scripts/stage-install.sh'],
       },
       build: {
         commands: [
           `./tools/build-workspace.sh ${appConfig.name} ${stage}`,
-          'git clone https://github.com/flutter/flutter.git -b stable --depth 1 /tmp/flutter',
           `yarn nx deploy crud-backend --stage=${stage} --app=${appConfig.name}`,
           `yarn deleteAllTableData`,
           `yarn seed`,
           `yarn nx deploy anyupp-backend --stage=${stage} --app=${appConfig.name}`,
-          'export PATH=$PATH:/tmp/flutter/bin',
-          'flutter doctor',
-          `yarn nx buildApk anyupp-mobile`,
+          `yarn nx buildApk-ci anyupp-mobile`,
         ],
       },
       post_build: {
