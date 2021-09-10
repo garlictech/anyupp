465cd9d6606bf0acfb0b4ec4c5a16c955d0324cf
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-09-10 09:18:23 +0200

    chore(anyupp-mobile): temporarily removing APP integration tests

--- apps/cicd/scripts/dev-post_build.sh
+++ apps/cicd/scripts/dev-post_build.sh
@@ -11,7 +11,7 @@ npx cowsay "TESTING $ENVNAME..."
 
 yarn nx test integration-tests-universal --codeCoverage --coverageReporters=clover
 yarn nx test integration-tests-angular --codeCoverage --coverageReporters=clover
-yarn nx run anyupp-mobile:integration-test-ci
+#yarn nx run anyupp-mobile:integration-test-ci
 yarn deleteAllTableData
 yarn seed
 yarn nx e2e-remote admin-e2e --headless --baseUrl=$ADMIN_SITE_URL
