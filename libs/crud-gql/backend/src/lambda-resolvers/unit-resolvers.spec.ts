672cb7cb3d66c461df6f96f0e3a2b3a0ad9b2491
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-09-23 15:58:20 +0200

    chore(crud-backend): update/create unit resolvers

--- libs/crud-gql/backend/src/lambda-resolvers/unit-resolvers.spec.ts
+++ libs/crud-gql/backend/src/lambda-resolvers/unit-resolvers.spec.ts
@@ -8,8 +8,15 @@ import { DynamoDB } from 'aws-sdk';
 import { unitFixture } from '@bgap/shared/fixtures';
 
 describe('Test custom CRUD resolvers', () => {
-  const getDeps = () => ({
-    hashGenerator: (param: string) => `HASHED ${param}`,
+  const getDeps = (): UnitResolverDeps => ({
+    hashGenerator: (param: string) => Promise.resolve(`HASHED ${param}`),
+    tableName: 'UNIT_TABLE_NAME',
+    docClient: new DynamoDB.DocumentClient(),
+    uuidGenerator: () => 'THE_UUID',
+  });
+
+  const getFailedDeps = (): UnitResolverDeps => ({
+    hashGenerator: (_param: string) => Promise.reject(`ERROR`),
     tableName: 'UNIT_TABLE_NAME',
     docClient: new DynamoDB.DocumentClient(),
     uuidGenerator: () => 'THE_UUID',
@@ -49,6 +56,11 @@ describe('Test custom CRUD resolvers', () => {
     expect(await createUnitResolver(deps)(fixture).toPromise()).toEqual(true);
     expect(putSpy.mock.calls).toMatchSnapshot('PUT CALL');
     expect(updateSpy).not.toHaveBeenCalled();
+
+    const failedDeps = getFailedDeps();
+    expect(createUnitResolver(failedDeps)(fixture).toPromise()).rejects.toEqual(
+      'ERROR',
+    );
   });
 
   test.each(cases)('updateUnitResolver tests', async fixture => {
@@ -61,5 +73,10 @@ describe('Test custom CRUD resolvers', () => {
     ).toEqual(true);
     expect(updateSpy.mock.calls).toMatchSnapshot('PUT CALL');
     expect(putSpy).not.toHaveBeenCalled();
+
+    const failedDeps = getFailedDeps();
+    expect(createUnitResolver(failedDeps)(fixture).toPromise()).rejects.toEqual(
+      'ERROR',
+    );
   });
 });
