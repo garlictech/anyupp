3f88292f0272ea871064668845b2b00ca34f9062
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-09-28 18:43:24 +0200

    chore(all): save prod amplify code and fix prod amplify deployment

--- apps/cicd/scripts/prod-build.sh
+++ apps/cicd/scripts/prod-build.sh
@@ -4,6 +4,8 @@ set -e
 ENVNAME=$1
 CI=$2
 
+# This is temporary solution!
+rm -rf apps/crud-backend/amplify
 ./tools/build-workspace.sh $ENVNAME $CI
 yarn nx deploy crud-backend --env=${ENVNAME}
 yarn nx deploy anyupp-backend --env=${ENVNAME}
