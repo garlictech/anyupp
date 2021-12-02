e906cf5ce312549d14611eda32c3c2dff66a365a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2021-12-02 02:25:45 +0100

    feat(anyupp-backend): fix some source analysis findings + re-add petrot stack

--- libs/backend/shared/utils/src/lib/utils.ts
+++ libs/backend/shared/utils/src/lib/utils.ts
@@ -1,13 +1,13 @@
 import * as sst from '@serverless-stack/resources';
 
-export const getFQParamName = (app: sst.App, paramName: string) =>
-  getFQParamNameFromDetails(app.stage, app.name, paramName);
-
 export const getFQParamNameFromDetails = (
   stage: string,
   appName: string,
   paramName: string,
 ) => `/${stage}-${appName}/generated/${paramName}`;
+
+export const getFQParamName = (app: sst.App, paramName: string) =>
+  getFQParamNameFromDetails(app.stage, app.name, paramName);
 
 export const commonBackendStack = 'common-backend-anyupp';
 export const anyuppVpcName = 'AnyuppVpc';
