94bcc813aef5ed82df266bf4daf4f087b07bf34a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-01-05 13:22:36 +0100

    chore(dev): add anyupp config

--- tools/config-anyupp-backend.sh
+++ tools/config-anyupp-backend.sh
@@ -9,6 +9,5 @@ STACKCONFIG_DIR=libs/shared/config/src/lib/stack-config
 STACKCONFIG_FILE=$STACKCONFIG_DIR/${ENVNAME}/stack-config.json
 
 if [ -f $STACKCONFIG_FILE ]; then
-  cd libs/shared/config/src/lib/generated
-  ln -sf ../stack-config/${ENVNAME}/stack-config.json .
+  cp $STACKCONFIG_FILE libs/shared/config/src/lib/generated/stack-config.json
 fi
--- tools/deploy-anyupp-backend.sh
+++ tools/deploy-anyupp-backend.sh
@@ -20,7 +20,6 @@ echo "Creating stack config for testing ($STACKCONFIG_FILE)"
 sed -i "s/^  \"${ENVNAME}\-anyupp\-backend/\"anyupp-backend/g" $STACKCONFIG_FILE
 
 if [ -f $STACKCONFIG_FILE ]; then
-  cd libs/shared/config/src/lib/generated
-  ln -sf ../stack-config/${ENVNAME}/stack-config.json .
+  cp $STACKCONFIG_FILE libs/shared/config/src/lib/generated/stack-config.json
 fi
 
