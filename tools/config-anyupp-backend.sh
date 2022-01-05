1404709d9225be53e23bf7d3a298ccc50523c097
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-01-05 13:54:56 +0100

    chore(dev): add anyupp config

--- tools/config-anyupp-backend.sh
+++ tools/config-anyupp-backend.sh
@@ -8,15 +8,7 @@ ENVNAME=$1
 STACKCONFIG_DIR=libs/shared/config/src/lib/stack-config
 STACKCONFIG_FILE=$STACKCONFIG_DIR/${ENVNAME}/stack-config.json
 
-echo "***********************************"
-echo $STACKCONFIG_FILE
-ls -l $STACKCONFIG_FILE
-
-
 if [ -f $STACKCONFIG_FILE ]; then
-  echo "COPYYING"
-  cp $STACKCONFIG_FILE libs/shared/config/src/lib/generated/stack-config.json
+  ln -sf $PWD/$STACKCONFIG_FILE $PWD/libs/shared/config/src/lib/generated/stack-config.json
 fi
 
-ls -l libs/shared/config/src/lib/generated/stack-config.json
-echo "***********************************"
