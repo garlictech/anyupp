f88c3d30d1c4f334e25be48387862c31cdefc384
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-01-17 12:26:38 +0100

    fix(backend): rkeeper fixes after tests

--- libs/rkeeper-api/src/lib/send-order.ts
+++ libs/rkeeper-api/src/lib/send-order.ts
@@ -4,40 +4,47 @@ import axios from 'axios';
 import { Method } from 'axios';
 import { defer, from } from 'rxjs';
 
-export const sendRkeeperOrder = (
-  unit: CrudApi.Unit,
-  orderInput: CrudApi.CreateOrderInput,
-) =>
-  pipe(
-    orderInput.items.map(item => ({
-      id: item.externalId,
-      type: 'd',
-      qnt: item.quantity * 1000,
-    })),
-    order => ({
-      objectid: unit.externalId,
-      order_type: 1,
-      pay_type:
-        orderInput.paymentMode.method === CrudApi.PaymentMethod.cash ? 0 : 1,
-      pay_online_type:
-        orderInput.paymentMode.method === CrudApi.PaymentMethod.cash ? 0 : 1,
-      delivery_time: Date(),
-      client: {
-        phone: unit.phone,
-        email: unit.email,
-        ln: 'Testln',
-        fn: 'Testfn',
-      },
-      order,
-    }),
-    data => ({
-      url: `${unit.pos?.rkeeper?.endpointUri}/postorder/${unit.externalId}`,
-      method: 'post' as Method,
-      data,
-      auth: {
-        username: unit.pos?.rkeeper?.rkeeperUsername || '',
-        password: unit.pos?.rkeeper?.rkeeperPassword || '',
-      },
-    }),
-    data => defer(() => from(axios.request(data))),
-  );
+export interface SendRkeeperOrderDeps {
+  currentTime: () => Date;
+}
+
+const defaultDeps: SendRkeeperOrderDeps = {
+  currentTime: () => new Date(),
+};
+
+export const sendRkeeperOrder =
+  (deps: SendRkeeperOrderDeps = defaultDeps) =>
+  (unit: CrudApi.Unit, orderInput: CrudApi.CreateOrderInput) =>
+    pipe(
+      orderInput.items.map(item => ({
+        id: item.externalId,
+        type: 'd',
+        qnt: item.quantity * 1000,
+      })),
+      order => ({
+        objectid: unit.externalId,
+        order_type: 1,
+        pay_type:
+          orderInput.paymentMode.method === CrudApi.PaymentMethod.cash ? 0 : 1,
+        pay_online_type:
+          orderInput.paymentMode.method === CrudApi.PaymentMethod.cash ? 0 : 1,
+        delivery_time: deps.currentTime().toISOString().split('.')[0],
+        client: {
+          phone: unit.phone,
+          email: unit.email,
+          ln: 'Testln',
+          fn: 'Testfn',
+        },
+        order,
+      }),
+      data => ({
+        url: `${unit.pos?.rkeeper?.endpointUri}/postorder/${unit.externalId}`,
+        method: 'post' as Method,
+        data,
+        auth: {
+          username: unit.pos?.rkeeper?.rkeeperUsername || '',
+          password: unit.pos?.rkeeper?.rkeeperPassword || '',
+        },
+      }),
+      data => defer(() => from(axios.request(data))),
+    );
