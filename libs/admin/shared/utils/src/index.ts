ed018ae5a469565409bc4d209410bf6e5b318ac7
Tamás Petró <petro.tamas@gmail.com>
2022-01-26 09:58:42 +0100

    QR code generation (#2647)
    * fix(admin): add canvas to package.json
    
    * fix(admin): cypress plugin config
    
    * feat(admin): package lock
    
    * feat(admin): report generator test update
    
    * feat(admin): qr wip
    
    * feat(admin): qr wip
    
    * feat(admin): print QR codes
    
    * feat(admin): zip unit qr codes
    
    * fix(admin): fix unit pos block
    
    * feat(admin): qr for fix
    
    * fix(admin): unit pos fix
    
    * feat(admin): qr generator test
    
    Co-authored-by: Zsolt R. Molnar <zsolt@zsoltmolnar.hu>

--- libs/admin/shared/utils/src/index.ts
+++ libs/admin/shared/utils/src/index.ts
@@ -1,7 +1,8 @@
 import { ImageCompressorService } from './lib/services/image-compressor/image-compressor.service';
+import { QrGeneratorService } from './lib/services/qr-generator/qr-generator.service';
 import { ToasterService } from './lib/services/toaster/toaster.service';
 
 export * from './lib/const';
 export * from './lib/enums';
 export * from './lib/fn';
-export { ImageCompressorService, ToasterService };
+export { ImageCompressorService, ToasterService, QrGeneratorService };
--- libs/admin/shared/utils/src/lib/fn/index.ts
+++ libs/admin/shared/utils/src/lib/fn/index.ts
@@ -2,4 +2,5 @@ export * from './dashboard';
 export * from './forms';
 export * from './orders';
 export * from './products';
+export * from './qr-generator';
 export * from './track-by-func';
