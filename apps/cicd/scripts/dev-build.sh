1e2a1654de44279355d620a70865e7f27537638e
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-01-26 11:47:41 +0100

    fix(cicd): deploy common backend only on DEV

--- apps/cicd/scripts/dev-build.sh
+++ apps/cicd/scripts/dev-build.sh
@@ -5,7 +5,11 @@ ENVNAME=$1
 CI=$2
 
 ./tools/build-workspace.sh $ENVNAME $CI
-yarn nx deploy common-backend --env=${ENVNAME}
+
+if [ "$ENVNAME" = "dev" ]; then
+  yarn nx deploy common-backend --env=${ENVNAME}
+fi
+
 yarn nx deploy crud-backend --env=${ENVNAME}
 yarn nx deploy anyupp-backend --env=${ENVNAME}
 yarn deleteAllTableData
