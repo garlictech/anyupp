3cc552907e4f6a3fc7b18cda0c80fd15b4ce9cc3
Asztalos Levente <36133889+jugamir@users.noreply.github.com>
2022-02-23 10:00:19 +0100

    Tooltip bug (#2925)
    * Iphone fixes, smaller tab space
    
    * Badge design fix
    
    * Fix qr background color
    
    * Multiple favorites bug fix
    
    * Narrow spinner
    
    * Transactions screen bug fix
    
    * Fix tipping form
    
    * Add missing lang key, order status only in full policy
    
    * Missing centertitle param
    
    * Color fixes, spelling fix
    
    * Allergens screen ios black padddings
    
    * fix spelling error
    
    Co-authored-by: Asztalos Levente <levente@bitgap.com>

--- apps/anyupp-mobile/lib/modules/menu/screens/product_menu_screen.dart
+++ apps/anyupp-mobile/lib/modules/menu/screens/product_menu_screen.dart
@@ -53,12 +53,13 @@ class _MenuState extends State<Menu> with TickerProviderStateMixin {
       bool? showed = preferences.getBool('TOOLTIP_${unit.id}');
       // print('_checkNeedToShowTooltip.showed=$showed');
       if (showed == null || showed == false) {
+        setState(() {
+          _showTooltip = true;
+        });
         await preferences.setBool('TOOLTIP_${unit.id}', true);
+      } else {
+        _showTooltip = false;
       }
-      setState(() {
-        _showTooltip = showed == null || showed == false;
-        // print('_checkNeedToShowTooltip._showTooltip=$_showTooltip');
-      });
     } else {
       setState(() {
         _showTooltip = false;
@@ -121,10 +122,10 @@ class _MenuState extends State<Menu> with TickerProviderStateMixin {
     _tabController?.addListener(() {
       if (_tabController?.indexIsChanging == false) {
         print('ProductMenuScreen.selectedTab()=${_tabController?.index}');
-        setState(() {
-          _selectedTab = _tabController!.index;
+        if (_showTooltip) {
           _checkNeedToShowTooltip();
-        });
+        }
+        _selectedTab = _tabController!.index;
       }
     });
 
