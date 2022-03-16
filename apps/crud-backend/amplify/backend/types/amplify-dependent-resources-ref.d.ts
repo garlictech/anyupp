2cb126048b114c2d8134e8c7d8f0eae332fb8e76
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-03-16 12:20:48 +0100

    chore(backend): re-add cleaned-up ampligy code

---
+++ apps/crud-backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "s312921516": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "anyuppbackend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
