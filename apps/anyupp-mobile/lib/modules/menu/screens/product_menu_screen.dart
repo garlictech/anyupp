d57f8bc5e62c49f31f1e8ec040864bc69c67b1b6
Bátori Gábor <batori.gabor@gmail.com>
2022-03-25 13:50:46 +0100

    fix: favorites not loaded on app (#3164)

--- apps/anyupp-mobile/lib/modules/menu/screens/product_menu_screen.dart
+++ apps/anyupp-mobile/lib/modules/menu/screens/product_menu_screen.dart
@@ -384,7 +384,6 @@ class _MenuState extends State<Menu> with TickerProviderStateMixin {
       child: AnimatedBuilder(
         animation: _tabController!.animation as Listenable,
         builder: (ctx, snapshot) {
-          print(_tabController?.offset.toString());
           final forward = _tabController!.offset > 0;
           final backward = _tabController!.offset < 0;
           int _fromIndex;
