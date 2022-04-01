3826d1839b21784e0874aab5796fa35e511c6b1f
Tamás Petró <petro.tamas@gmail.com>
2022-04-01 16:23:23 +0200

    Qr pdf (#3197)
    * fix(admin): remove kajahu url
    
    * fix(admin): dnapshot fix
    
    * feat(admin): virtual scroll fixes
    
    * feat(admin): e2e auth fix
    
    * fix(admin): import fixes
    
    * feat(admin): pull upstream
    
    * feat(admin): tamasstack
    
    * feat(admin): platform config
    
    * fix(admin): e2e dashboard fix
    
    * fix(admin): e2e dashboard fix
    
    * fix(admin):  URL updates (QR, RKeeper)
    
    * fix(admin):  URL updates (QR, RKeeper)
    
    * fix(admin): build fix
    
    * feat(admin): stripe merchant id
    
    * fix(admin): e2e fix
    
    * feat(admin): pull
    
    * fix(admin): snapshot fix
    
    * feat(admin): qr pdf
    
    * feat(admin): refactor qr generator logic
    
    Co-authored-by: Zsolt R. Molnar <zsolt@zsoltmolnar.hu>

--- libs/integration-tests/angular/src/lib/libs/admin/shared/utils/qr-generator.spec.ts
+++ libs/integration-tests/angular/src/lib/libs/admin/shared/utils/qr-generator.spec.ts
@@ -1,7 +1,11 @@
-import { getQR } from '@bgap/admin/shared/utils';
+import { getPDF, getQR } from '@bgap/admin/shared/utils';
 
 describe('QrGenerator', () => {
   it('getQR should create SVG', async () => {
     expect(await getQR('unitId', 'tableId', 'seatId')).toMatchSnapshot();
+  });
+
+  it('getPDF should create QR PDF', async () => {
+    expect(await getPDF('mock_QR_SVG_content')).toMatchSnapshot();
   });
 });
