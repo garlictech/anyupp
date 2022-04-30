58e38edd7f9a3cc6ea1548760c7e348f6d67e6cc
gbatori <batori.gabor@gmail.com>
2022-04-30 11:26:20 +0200

    feat: user profile details screen with guest label

--- apps/anyupp-mobile/test/unit/md5_hash_test.dart
+++ apps/anyupp-mobile/test/unit/md5_hash_test.dart
@@ -4,7 +4,7 @@ import 'package:fa_prev/shared/utils/md5_hash.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  void checkHashExpected(String source, String expected, [bool debug = true]) {
+  void checkHashExpected(String source, String expected, [bool debug = false]) {
     String hash = generateHash(source);
     if (debug) print('source: $source, hash: $hash');
     expect(hash, equals(expected));
@@ -14,11 +14,11 @@ void main() {
     test('Basic generation', () async {
       checkHashExpected(
         'f90e05c0-c146-11ec-96df-69d007d461d4',
-        'c5124f',
+        'C5124F',
       );
       checkHashExpected(
         '009a0c01-bbf5-11ec-9b13-bbc9692ef9aa',
-        '5c7dda',
+        '5C7DDA',
       );
     });
   });
