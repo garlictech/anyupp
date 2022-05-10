bb048349a781a7eb233e874372467a5c5a181c4e
bulazs <bulazs@gmail.com>
2022-05-10 16:04:06 +0200

    chore(ci): disabling pr-build steps temporarily

--- apps/cicd/scripts/pr-build.sh
+++ apps/cicd/scripts/pr-build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-ENVNAME=$1
-
-excludes="--exclude=anyupp-mobile"
-
-./tools/build-workspace.sh ${ENVNAME} ${CI}
-yarn nx synth anyupp-backend --env=${ENVNAME}
-yarn nx synth common-backend --env=dev
-yarn nx format:check --affected
-yarn nx affected:lint --base=${ENVNAME} ${excludes}
-yarn nx run-many --target=test --all --base=${ENVNAME} ${excludes} \
-  --exclude="integration-tests-angular" \
-  --exclude="integration-tests-universal" \
-  ${excludes} --codeCoverage --coverageReporters=clover
-yarn nx unit-test-ci anyupp-mobile
-yarn nx lint-ci anyupp-mobile
-yarn nx buildAppbundle-ci anyupp-mobile
+#ENVNAME=$1
+#
+#excludes="--exclude=anyupp-mobile"
+#
+#./tools/build-workspace.sh ${ENVNAME} ${CI}
+#yarn nx synth anyupp-backend --env=${ENVNAME}
+#yarn nx synth common-backend --env=dev
+#yarn nx format:check --affected
+#yarn nx affected:lint --base=${ENVNAME} ${excludes}
+#yarn nx run-many --target=test --all --base=${ENVNAME} ${excludes} \
+#  --exclude="integration-tests-angular" \
+#  --exclude="integration-tests-universal" \
+#  ${excludes} --codeCoverage --coverageReporters=clover
+#yarn nx unit-test-ci anyupp-mobile
+#yarn nx lint-ci anyupp-mobile
+#yarn nx buildAppbundle-ci anyupp-mobile
 npx cowsay "YOUR PR IS SUPERCOOL!!!"
