7b838454a64fe64f89af762b8ce2c7b6cb509304
Tamás Petró <petro.tamas@gmail.com>
2022-05-11 14:29:14 +0200

    fix(admin): initial menu highlight

--- libs/admin/ui/theme/src/lib/components/admin-layout/admin-layout.component.ts
+++ libs/admin/ui/theme/src/lib/components/admin-layout/admin-layout.component.ts
@@ -2,7 +2,6 @@ import { debounceTime, map, take } from 'rxjs/operators';
 
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { environment } from '@bgap/admin/shared/config';
-import { MENU_ROLES } from '@bgap/admin/shared/utils';
 import { appCoreSelectors } from '@bgap/admin/store/app-core';
 import { loggedUserSelectors } from '@bgap/admin/store/logged-user';
 import * as CrudApi from '@bgap/crud-gql/api';
@@ -12,6 +11,7 @@ import { NbMenuItem } from '@nebular/theme';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 const menuItems = {
   dashboard: {
@@ -19,66 +19,42 @@ const menuItems = {
     icon: { icon: 'home', pack: 'material-icons' },
     link: '/admin/dashboard',
     home: true,
-    roles: MENU_ROLES.DASHBOARD,
   },
   products: {
     title: 'menu.products',
     icon: { icon: 'book', pack: 'material-icons' },
     link: '/admin/products',
-    roles: MENU_ROLES.PRODUCTS,
   },
   productCategories: {
     title: 'menu.productCategories',
     icon: { icon: 'local_bar', pack: 'material-icons' },
     link: '/admin/product-categories',
-    roles: MENU_ROLES.PRODUCT_CATEGORIES,
   },
   productModifiers: {
     title: 'menu.modifiersAndExtras',
     icon: { icon: 'domain', pack: 'material-icons' },
     link: '/admin/modifiers-and-extras',
-    roles: MENU_ROLES.MODIFIERS_AND_EXTRAS,
   },
   units: {
     title: 'menu.units',
     icon: { icon: 'store', pack: 'material-icons' },
     link: '/admin/units',
-    roles: MENU_ROLES.UNITS,
   },
   groups: {
     title: 'menu.groups',
     icon: { icon: 'group_work', pack: 'material-icons' },
     link: '/admin/groups',
-    roles: MENU_ROLES.GROUPS,
   },
   chains: {
     title: 'menu.chains',
     icon: { icon: 'stars', pack: 'material-icons' },
     link: '/admin/chains',
-    roles: MENU_ROLES.CHAINS,
   },
-  /*
-  users: {
-    title: 'menu.users',
-    icon: { icon: 'people', pack: 'material-icons' },
-    link: '/admin/users',
-    roles: MENU_ROLES.USERS,
-  },
-  */
   admins: {
     title: 'menu.admins',
     icon: { icon: 'people', pack: 'material-icons' },
     link: '/admin/admins',
-    roles: MENU_ROLES.ADMINS,
   },
-  /*
-  roleContexts: {
-    title: 'menu.roleContexts',
-    icon: { icon: 'verified_user_outline', pack: 'material-icons' },
-    link: '/admin/role-contexts',
-    roles: MENU_ROLES.ROLE_CONTEXTS,
-  },
-  */
 };
 
 @UntilDestroy()
@@ -96,6 +72,7 @@ export class AdminLayoutComponent implements OnInit, OnDestroy {
     private _store: Store,
     private _translateService: TranslateService,
     private _changeDetectorRef: ChangeDetectorRef,
+    private _router: Router,
   ) {}
 
   ngOnInit() {
@@ -115,6 +92,7 @@ export class AdminLayoutComponent implements OnInit, OnDestroy {
             this.menu.push({
               ...menuItem,
               title: this._translateService.instant(menuItem.title),
+              selected: this._router.url.includes(menuItem.link),
             });
           }
         });
