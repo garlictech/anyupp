6a825d6e9962dfd9130491dd410e19e0f0bf6313
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-06-16 11:53:32 +0200

    chore(tools): place nx cache somewhere else

--- apps/cicd/scripts/pr-build.sh
+++ apps/cicd/scripts/pr-build.sh
@@ -5,16 +5,29 @@ ENVNAME=$1
 
 excludes="--exclude=anyupp-mobile"
 
+rm -rf /tmp/nx
 ./tools/build-workspace.sh ${ENVNAME} ${CI}
+echo "*******************"
+du -sh /tmp/nx
+echo "*******************"
 yarn nx synth anyupp-backend --env=${ENVNAME}
 yarn nx synth common-backend --env=dev
 yarn nx format:check --affected
+echo "*******************"
+du -sh /tmp/nx
+echo "*******************"
 export DEBUG=*
 yarn nx affected:lint --base=dev ${excludes}
+echo "*******************"
+du -sh /tmp/nx
+echo "*******************"
 yarn nx affected:test --base=dev ${excludes} \
   --exclude="integration-tests-angular" \
   --exclude="integration-tests-universal" \
   ${excludes} --codeCoverage --coverageReporters=clover
+echo "*******************"
+du -sh /tmp/nx
+echo "*******************"
 yarn nx unit-test-ci anyupp-mobile
 yarn nx lint-ci anyupp-mobile
 yarn nx buildAppbundle-ci anyupp-mobile
