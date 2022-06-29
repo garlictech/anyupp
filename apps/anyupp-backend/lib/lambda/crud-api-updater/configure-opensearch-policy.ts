7487529170c4b03ad21027f7d90faf1be58334b0
Mihály Balogh <101461480+onlineforge@users.noreply.github.com>
2022-06-29 19:25:49 +0200

    Os policy updater (#3659)
    * feat(infra): update os access policy in crud-api-updater
    
    * fix(infra): only try to add access policy when advancedSecurity is enabled
    
    * fix(infra): reformat code
    
    * fix(infra): only try to add access policy when advancedSecurity is enabled
    
    Co-authored-by: Zsolt R. Molnar <zsolt@zsoltmolnar.hu>

--- apps/anyupp-backend/lib/lambda/crud-api-updater/configure-opensearch-policy.ts
+++ apps/anyupp-backend/lib/lambda/crud-api-updater/configure-opensearch-policy.ts
@@ -1,11 +1,11 @@
 import { OpenSearch } from 'aws-sdk';
 import { CrudApiConfig } from '@bgap/crud-gql/api';
 import { throwIfEmptyValue } from '@bgap/shared/utils';
-import { defer, from, of, throwError } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { defer, from, of } from 'rxjs';
+import { catchError, filter, map, switchMap } from 'rxjs/operators';
 
 const openSearch = new OpenSearch({
-  apiVersion: '2017-07-25',
+  apiVersion: '2021-01-01',
 });
 
 const domainName = CrudApiConfig.openSearchEndpoint.split('/')[1];
@@ -31,6 +31,9 @@ export const configureOpenSearchPolicy = () =>
     from(openSearch.describeDomain({ DomainName: domainName }).promise()),
   ).pipe(
     map(response => response.DomainStatus),
+    filter(
+      domainStatus => domainStatus.AdvancedSecurityOptions?.Enabled == true,
+    ),
     throwIfEmptyValue(),
     switchMap(() =>
       from(
@@ -42,9 +45,8 @@ export const configureOpenSearchPolicy = () =>
           .promise(),
       ),
     ),
-    catchError(err =>
-      JSON.stringify(err, null, 2).includes('OpenSearch access policies')
-        ? of(true)
-        : throwError(err),
-    ),
+    catchError(err => {
+      console.warn('Policy attachment was not successful: ', err);
+      return of({});
+    }),
   );
