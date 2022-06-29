319b934ebed5d52ab394a1fc9ecfd706d9f0eea1
Mihaly Balogh <mihaly@onlineforge.cloud>
2022-06-29 13:10:32 +0200

    feat(infra): add global waf webacl

--- apps/anyupp-backend/lib/app/cloudfront-waf-stack.ts
+++ apps/anyupp-backend/lib/app/cloudfront-waf-stack.ts
@@ -1,16 +1,17 @@
 import * as sst from '@serverless-stack/resources';
-import { Stack, StackProps } from '@serverless-stack/resources';
+import { StackProps } from '@serverless-stack/resources';
 import { WebAclWithRules } from '../shared/WebAclWithRules';
 
 export class WafStack extends sst.Stack {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(scope: sst.App, id: string, props?: StackProps) {
     super(scope, id);
+    props?.env;
 
     new WebAclWithRules(this, 'frontendAcl', {
       namePostfix: '',
-      region: Stack.of(this).region,
-      aclType: 'REGIONAL',
+      region: 'us-east-1',
+      aclType: 'CLOUDFRONT',
     });
   }
 }
