0fd9c3fb28f4d41aa4f7499f88ad29c9294ba413
Mihaly Balogh <mihaly@onlineforge.cloud>
2022-07-07 13:40:46 +0200

    feat(infra): fix linter issue

--- apps/anyupp-global/lib/app/cf-waf-stack.ts
+++ apps/anyupp-global/lib/app/cf-waf-stack.ts
@@ -4,6 +4,8 @@ import { Stack, StackProps } from '@serverless-stack/resources';
 import { WebAclWithRules } from '../shared/webacl-with-rules';
 
 export const GLOBAL_WAF_ACL_ARN_PARAM_NAME = 'GLOBAL_WAF_ACL_ARN';
+
+//(@typescript-eslint/no-unused-vars)
 export class WafStack extends Stack {
   constructor(scope: App, id: string, props: StackProps) {
     super(scope, id);
