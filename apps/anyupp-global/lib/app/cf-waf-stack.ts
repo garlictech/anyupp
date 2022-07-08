e3e1765a559b39e5e52437bf645d66aa71735b05
Mihaly Balogh <mihaly@onlineforge.cloud>
2022-07-08 09:57:27 +0200

    feat(infra): fix waf resource names

--- apps/anyupp-global/lib/app/cf-waf-stack.ts
+++ apps/anyupp-global/lib/app/cf-waf-stack.ts
@@ -3,15 +3,14 @@ import { App } from '@serverless-stack/resources';
 import { Stack, StackProps } from '@serverless-stack/resources';
 import { WebAclWithRules } from '../shared/webacl-with-rules';
 
-export const GLOBAL_WAF_ACL_ARN_PARAM_NAME = 'GLOBAL_WAF_ACL_ARN';
-
 export class WafStack extends Stack {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(scope: App, id: string, props: StackProps) {
     super(scope, id);
+    const app = this.node.root as App;
 
     const webAcl = new WebAclWithRules(this, 'frontendAcl', {
-      namePostfix: '',
+      namePrefix: app.stageName,
       region: Stack.of(this).region,
       aclType: 'CLOUDFRONT',
     });
@@ -19,7 +18,7 @@ export class WafStack extends Stack {
     new StringParameter(this, 'GlobalWafAclArnParam', {
       stringValue: webAcl.webAcl.attrArn,
       description: 'Global WAF Web ACL Arn deployed in us-east-1',
-      parameterName: GLOBAL_WAF_ACL_ARN_PARAM_NAME,
+      parameterName: `${scope.stage}_GLOBAL_WAF_ACL_ARN`,
     });
   }
 }
