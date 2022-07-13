eff0c14cd04dd7614a01310e52c90888810a4692
bulazs <bulazs@gmail.com>
2022-07-13 10:24:07 +0200

    chore(infra): 3720 - Add UserAgent header to typescript (CrudSDK) API calls

--- libs/shared/graphql/api-client/src/lib/graphql-api-client.ts
+++ libs/shared/graphql/api-client/src/lib/graphql-api-client.ts
@@ -1,9 +1,16 @@
 import { ApolloQueryResult, QueryOptions } from 'apollo-client';
-import AWSAppSyncClient, { AWSAppSyncClientOptions } from 'aws-appsync/lib';
+import AWSAppSyncClient, {
+  AWSAppSyncClientOptions,
+  createAppSyncLink,
+} from 'aws-appsync/lib';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink, FetchResult } from 'apollo-link';
 import { DocumentNode } from 'graphql';
 import { from, Observable } from 'rxjs';
-import { FetchResult } from 'apollo-link';
 import { CrudApiConfig } from './types';
+
+const CUSTOM_USER_AGENT = 'GraphqlApiClient';
 
 export class GraphqlApiClient {
   public _client: AWSAppSyncClient<Record<string, never>>;
@@ -12,12 +19,31 @@ export class GraphqlApiClient {
     genericConfig: CrudApiConfig,
     specificConfig: Partial<AWSAppSyncClientOptions>,
   ) {
-    this._client = new AWSAppSyncClient({
+    const AppSyncConfig = {
       url: genericConfig.aws_appsync_graphqlEndpoint,
       region: genericConfig.aws_appsync_region,
       ...specificConfig,
       cacheOptions: {},
-    } as AWSAppSyncClientOptions);
+      customUserAgent: 'my-process-name',
+    } as AWSAppSyncClientOptions;
+
+    this._client = new AWSAppSyncClient(AppSyncConfig, {
+      link: createAppSyncLink({
+        ...AppSyncConfig,
+        resultsFetcherLink: ApolloLink.from([
+          setContext((request, previousContext) => ({
+            headers: {
+              ...previousContext.headers,
+              'user-agent': CUSTOM_USER_AGENT,
+            },
+          })),
+          createHttpLink({
+            uri: AppSyncConfig.url,
+          }),
+        ]),
+        complexObjectsCredentials: () => null,
+      }),
+    });
   }
 
   query<INPUT, OUTPUT>(
