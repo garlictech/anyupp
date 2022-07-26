b51b3830f9d47f80fabb1af214db8978a470b5a2
codefactor-io <support@codefactor.io>
2022-07-26 07:47:39 +0000

    [CodeFactor] Apply fixes
    [ci skip] [skip ci]

--- libs/admin/refactor/src/lib/store/products/+state/products.reducer.ts
+++ libs/admin/refactor/src/lib/store/products/+state/products.reducer.ts
@@ -2,4 +2,4 @@ import { ChainProduct, GroupProduct, UnitProduct } from '@bgap/domain';
 
 export interface ExtendedGroupProduct extends ChainProduct, GroupProduct {}
 
-export interface ExtendedUnitProduct extends UnitProduct {}
+export type ExtendedUnitProduct = UnitProduct
--- libs/shared/fixtures/src/lib/product-snapshot.ts
+++ libs/shared/fixtures/src/lib/product-snapshot.ts
@@ -4,7 +4,6 @@ import {
   CreateUnitProductInput,
   ProductType,
   ServingMode,
-  UnitProduct,
 } from '@bgap/domain';
 import { seededIdPrefix } from './common';
 
