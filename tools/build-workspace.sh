367886781db0056d7fd30d4b9dc51de4e2873fc3
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-08-10 15:48:42 +0200

    chore(tools): amplify env fix

--- tools/build-workspace.sh
+++ tools/build-workspace.sh
@@ -9,20 +9,20 @@ AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.eu-west-1.amazonaws.com
 
 yarn nx run-many --target=config --all --skip-nx-cache --env=$ENVNAME
-yarn nx run-many --target=build --projects=crud-backend,anyupp-global,anyupp-backend,anyupp-mobile --env=$ENVNAME --mode=$MODE --skip-nx-cache
+yarn nx run-many --target=build --projects=crud-backend,anyupp-global,anyupp-backend,anyupp-mobile --env=$ENVNAME --mode=$MODE
 
 if [ $MODE = 'ci' ]; then
-  yarn nx run-many --target=build-ci --projects=variants-manager-site --skip-nx-cache
+  yarn nx run-many --target=build-ci --projects=variants-manager-site --env=$ENVNAME
 else
-  yarn nx run-many --target=build --projects=variants-manager-site --skip-nx-cache
+  yarn nx run-many --target=build --projects=variants-manager-site --env=$ENVNAME
 fi
 
 if [ $ENVNAME = 'dev' ]; then
-  yarn nx build admin --skip-nx-cache 
+  yarn nx build admin --env=$ENVNAME
 elif [ $ENVNAME = 'prod' ]; then
-  yarn nx build admin --configuration=production --skip-nx-cache 
+  yarn nx build admin --configuration=production --env=$ENVNAME 
 elif [ $ENVNAME = 'qa' ] || [ $ENVNAME = 'staging' ]; then
-  yarn nx build admin --configuration=$ENVNAME --skip-nx-cache
+  yarn nx build admin --configuration=$ENVNAME --env=$ENVNAME
 else
-  yarn nx build admin --skip-nx-cache
+  yarn nx build admin --env=$ENVNAME
 fi
