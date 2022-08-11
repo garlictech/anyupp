6e1f7a166988b603d3a8fed9d5e2358d59102754
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-08-11 09:10:16 +0200

    chore(tools): workspace build fix

--- tools/build-workspace.sh
+++ tools/build-workspace.sh
@@ -12,17 +12,17 @@ yarn nx run-many --target=config --all --skip-nx-cache --env=$ENVNAME
 yarn nx run-many --target=build --projects=crud-backend,anyupp-global,anyupp-backend,anyupp-mobile --env=$ENVNAME --mode=$MODE
 
 if [ $MODE = 'ci' ]; then
-  yarn nx run-many --target=build-ci --projects=variants-manager-site --env=$ENVNAME
+  yarn nx run-many --target=build-ci --projects=variants-manager-site
 else
-  yarn nx run-many --target=build --projects=variants-manager-site --env=$ENVNAME
+  yarn nx run-many --target=build --projects=variants-manager-site
 fi
 
 if [ $ENVNAME = 'dev' ]; then
-  yarn nx build admin --env=$ENVNAME
+  yarn nx build admin
 elif [ $ENVNAME = 'prod' ]; then
-  yarn nx build admin --configuration=production --env=$ENVNAME 
+  yarn nx build admin --configuration=production 
 elif [ $ENVNAME = 'qa' ] || [ $ENVNAME = 'staging' ]; then
-  yarn nx build admin --configuration=$ENVNAME --env=$ENVNAME
+  yarn nx build admin --configuration=$ENVNAME
 else
-  yarn nx build admin --env=$ENVNAME
+  yarn nx build admin
 fi
