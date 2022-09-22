7cb3f238122548e629254e1531c284f93e8135b7
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-09-22 18:10:46 +0200

    fix(admin): qr related improvments

--- libs/admin/refactor/src/lib/shared/utils/services/qr-generator/qr-generator.service.ts
+++ libs/admin/refactor/src/lib/shared/utils/services/qr-generator/qr-generator.service.ts
@@ -1,6 +1,6 @@
 import { saveAs } from 'file-saver';
 import JSZip from 'jszip';
-import { from } from 'rxjs';
+import { from, of } from 'rxjs';
 import { concatMap, last, switchMap, tap } from 'rxjs/operators';
 
 import { Injectable } from '@angular/core';
@@ -31,15 +31,24 @@ export class QrGeneratorService {
           ),
         ),
         last(),
-        switchMap(() => seatObjects),
-        concatMap(item =>
-          from(getQR(unit.id, item.tID, item.sID)).pipe(
-            tap(qrStr => zip.file(`t${item.tID}-s${item.sID}.svg`, qrStr)),
-            switchMap(qrStr => from(getPDF(qrStr))),
-            tap(pdf => zip.file(`t${item.tID}-s${item.sID}.pdf`, pdf.output())),
-          ),
+        switchMap(() =>
+          seatObjects.length == 0
+            ? of({})
+            : from(seatObjects).pipe(
+                concatMap(item =>
+                  from(getQR(unit.id, item.tID, item.sID)).pipe(
+                    tap(qrStr =>
+                      zip.file(`t${item.tID}-s${item.sID}.svg`, qrStr),
+                    ),
+                    switchMap(qrStr => from(getPDF(qrStr))),
+                    tap(pdf =>
+                      zip.file(`t${item.tID}-s${item.sID}.pdf`, pdf.output()),
+                    ),
+                  ),
+                ),
+                last(),
+              ),
         ),
-        last(),
         tap(() => {
           zip.generateAsync({ type: 'blob' }).then(blob => {
             saveAs(blob, `${unit.name} QR Codes.zip`);
