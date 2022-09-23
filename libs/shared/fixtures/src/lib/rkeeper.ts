2d83350f9a4c118529af461f375de1a60d3229b4
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-09-23 22:19:43 +0200

    chore(test): rkeeper test + parameter upgrades

--- libs/shared/fixtures/src/lib/rkeeper.ts
+++ libs/shared/fixtures/src/lib/rkeeper.ts
@@ -2,3 +2,4 @@ export const freiRestaurantId = '109150001';
 export const rkeeperEndpoint = `https://testendpoint.ucs.hu/wp-json/vendor/v1`;
 export const freiRkeeperUsername = '795_50_155_539';
 export const freiRkeeperPassword = 'b6302d53085c9486d0f765ec475f18';
+export const freiRkeeperWaiterProductId = '1040917';
