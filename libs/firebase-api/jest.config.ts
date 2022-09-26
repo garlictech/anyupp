157c9699608bd572895d8ff7fdda006b5ad1e81a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2022-09-27 01:00:23 +0200

    chore(all): integrating firebase admin to the system

--- libs/firebase-api/jest.config.ts
+++ libs/firebase-api/jest.config.ts
@@ -11,6 +11,9 @@ export default {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    'firebase-admin/(.*)': '<rootDir>/mocks/empty-mock.js',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/firebase-api',
 };
